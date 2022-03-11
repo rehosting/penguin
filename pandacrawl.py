e20303929dfa292ed9de1fdaaa95c654b632e561
Andrew Fasano <fasano@mit.edu>
2022-03-11 10:24:49 -0500

    Start of new class, no updates to old code yet

--- pandacrawl.py
+++ pandacrawl.py
@@ -10,11 +10,15 @@ from time import sleep
 from bs4 import BeautifulSoup
 import coloredlogs
 
+import pickle
+import glob
 import threading
+import json
 from queue import Queue
 from threading import Lock
 import requests
 from vsockadapter import VSockAdapter
+from urlparse import urlparse
 
 from pandare import PyPlugin
 from pandarepyplugins import CallTree
@@ -48,6 +52,98 @@ if __name__ == '__main__':
     assert (v1 == "foo/boo.html"), v1
 '''
 
+def _strip_url(url):
+    '''
+    Remove any redundant properties from URLs
+
+    Remove duplicate /s and anything after #
+
+    Also remove anything after ? - TODO: get params might be part of form attack surface we want to fuzz
+    so maybe we should add these to the fuzz queue
+    '''
+
+    if "://" in url:
+        meth, body = url.split("://")
+    else:
+        meth = None
+        body = url
+
+    while "//" in body:
+        body = body.replace("//", "/")
+
+    if "#" in body:
+        body = body.split("#")[0]
+
+    if "?" in body:
+        # TODO: maybe add to fuzz queue as a GET target?
+        body = body.split("?")[0]
+
+    if body.startswith("/"):
+        body = body[1:]
+
+    if meth:
+        return meth + "://" + body
+    return body
+
+
+class PageDetails(object):
+    '''
+    Given a URL and method, store info in a standardized format
+    '''
+
+    def __init__(self, full_url, method="GET"):
+        '''
+        Provides details of URL:
+         self.info.{hostname,port,path,query,fragement}
+         self.method
+        '''
+        self.info = urlparse(full_url)
+        self.method = method
+
+    def __eq__(self, other)
+        # TODO: does comparison of .info work?
+        return self.method == other.method and self.info == other.info
+
+class TargetInfo(object):
+    '''
+    For a given service, track details of observed pages
+    and details of yet-to-be-visited pages.
+
+    Maybe needs to be thread-safe
+    '''
+
+    def __init__(self, family, ip, port, service_name=None):
+        self.results = {} # PageDetails -> Response
+
+        self.session = None # Session object for requests to use
+
+        # Queue of pages to be visited
+        self.pending = Queue()
+
+        # List of pages that have been visited
+        self.visited = []
+
+        # Set of known pages
+        self.pages = set()
+
+        # Queue of forms to 
+        self.pending_forms = Queue()
+        self.froms = set()
+
+    def get_next_page(self):
+        '''
+        Get the next page to visit.
+        '''
+        pass
+
+    def store_and_parse_response(self, request, response):
+        '''
+        Store the response for the given request
+        and parse it for more URLs to queue up
+        '''
+        pass
+
+
 class PandaCrawl(PyPlugin):
     '''
     Track vsockify listening services in a guest. Identify services
@@ -58,7 +154,13 @@ class PandaCrawl(PyPlugin):
         self.logger = logging.getLogger('PandaCrawl')
         self.cid = int(self.get_arg("cid"))
 
-        self.targets = {} # (service_name, family, ip, port): {pending: {}, visited: {}, pages: set(), forms: {}}
+        self.targets = {} # (service_name, family, ip, port): {
+                          #     pending: {},        
+                          #     visited: {},
+                          #     pages: set(),
+                          #     pending_forms: {} (path, method, json(params))
+                          #     forms: set() => (path, method)
+                          #}
         self.have_targets = Lock()
         self.have_targets.acquire() # Lock it immediately
 
@@ -80,9 +182,26 @@ class PandaCrawl(PyPlugin):
         if key not in self.targets:
             self.targets[key] = {'pending': Queue(),
                     'visited': [], 'pages': set(),
-                    'forms': [], 'session': None}
+                    'pending_forms': set(), 'forms': set(),
+                    'session': None}
 
-            self.targets[key]['pending'].put("/")
+            # Read all target_X.pickle files to see if any match this
+            # if so, seed state with that
+            for possible_pickle in glob.glob("./target_*.pickle"):
+                with open(possible_pickle, "rb") as f:
+                    (targ_info, pending_q, pages, visited, pending_forms, forms) = pickle.load(f)
+
+                if targ_info == key:
+                    self.logger.warning("Loading data from cache")
+                    [self.targets[key]["pending"].put(x) for x in pending_q]
+                    self.targets[key]["pages"] = pages
+                    self.targets[key]["visited"] = visited
+                    self.targets[key]["pending_forms"] = pending_forms
+                    self.targets[key]["forms"] = forms
+                    break
+            else:
+                # Didn't break
+                self.targets[key]['pending'].put("/")
 
             if len(self.targets) == 1 and self.have_targets.locked():
                 print("unlock have_targets")
@@ -143,6 +262,27 @@ class PandaCrawl(PyPlugin):
             #self.logger.info(f"Targeting {target_key}: {url} (path={path}) with session {session}")
 
             self.crawl_one(session, url, target_key)
+            #break # XXX DEBUG
+
+        self.logger.info("Finished crawling")
+        for idx, targ_info in enumerate(self.targets.keys()):
+            details = self.targets[targ_info]
+            try:
+                with open(f"target_{idx}.pickle", "wb") as f:
+                    pending_q = []
+                    while not details['pending'].empty():
+                        pending_q.append(details['pending'].get())
+
+                    pickle.dump((targ_info,
+                        pending_q, details['pages'], details['visited'],
+                        details['pending_forms'], details['forms']), f)
+            except Exception as e:
+                print("ERROR PICKLING:", e)
+                raise
+
+        print("\n\nDONE")
+        import sys
+        sys.exit()
 
     def crawl_one(self, session, url, target_key):
         '''
@@ -165,7 +305,7 @@ class PandaCrawl(PyPlugin):
             this_target['pending'].put(targ_url)
 
     def analyze_response(self, target_key, url, request):
-        self.logger.info(f"Analyzing {url} => {repr(request.text[:500])}")
+        self.logger.info(f"Analyzing {url} ({len(self.targets[target_key]['pages'])}of {self.targets[target_key]['pending'].qsize()} => {repr(request.text[:500])}")
 
         if request.status_code == 401:
             self.logger.error("UNAUTHORIZED - retrying with admin/admin") # TODO: better
@@ -187,6 +327,8 @@ class PandaCrawl(PyPlugin):
             for match in re.findall(f'src=.([a-zA-Z0-9._/]*).', request.text):
                 self.add_ref(target_key, url, match)
 
+        if url.endswith(".png") or url.endswith(".jpg"):
+            pass
         else:
             # Fallthrough: HTML
             raw_html = request.text
@@ -205,4 +347,60 @@ class PandaCrawl(PyPlugin):
 
             for form in soup.findAll('form'):
                 print(f"TODO: parse form {repr(form)}")
-                #self.parse_form(form)
+                self.parse_form(form, url, target_key)
+
+    def parse_form(self, bs_form, url, target_key):
+        '''
+        Given a bs4.form, generate permutations of submitting it and add to queue
+
+        Generates a simple dict to represent the form
+
+        TODO: combine with some static analysis to identify additional params
+        '''
+        raw_act = bs_form.get('action') # may be none if it should post to current page
+        abs_act = make_abs(url, _strip_url(raw_act) if raw_act else "")
+
+        raw_meth = bs_form.get('method')
+        meth = raw_meth.upper() if raw_meth else 'GET'
+        form = {
+                "action": abs_act,
+                "method": meth, # always caps
+                "params": {}
+        }
+
+        for field in bs_form.findAll('input'):
+            name = field.get('name')
+            if not name:
+                continue
+
+            if name not in form['params']:
+                form['params'][name] = {
+                        'type':    field.get('type'),
+                        'defaults': [field.get('value')]
+                        }
+            else:
+                # Duplicate name - e.g., a set of radio buttons. Add to defaults
+                form['params'][name]['defaults'].append(field.get('value'))
+
+        self.logger.info(f"Recording form that {form['method']}s to {abs_act}")
+        self.do_form_queue(abs_act, form, target_key)
+
+    def do_form_queue(self, path, form, target_key):
+        '''
+        A form object must capture method and params.
+
+        We'll submit a bunch of junk to it later
+        '''
+
+        for k in ["method", "params"]: # No need for action, we have path
+            if k not in form.keys():
+                raise ValueError(f"Forms to be queued must have {k}")
+
+        method = form['method']
+        path = _strip_url(path)
+
+        if (path, method) not in self.targets[target_key]['forms']:
+            # TODO: What if we find additional fields later? Should merge if exists
+            # but params are different
+            self.targets[target_key]['pending_forms'].add((path, method, json.dumps(form['params'])))
+            self.targets[target_key]['forms'].add((path, method))
