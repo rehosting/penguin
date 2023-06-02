b470d44f81da4e4565f7091e9e3843bad89b9572
Andrew Fasano <fasano@mit.edu>
2023-06-02 11:06:20 -0400

    Coverage, actually track. But in python

--- coverage.py
+++ coverage.py
@@ -1,4 +1,5 @@
 # Hypercall based coverage tracking
+import copy
 from pandare import PyPlugin
 
 class Coverage(PyPlugin):
@@ -7,10 +8,25 @@ class Coverage(PyPlugin):
         self.outdir = self.get_arg("outdir")
         self.in_vma_loop = False
         self.pending_proc = {}
-        self.vmas = []
         self.ppp_cb_boilerplate('on_hc_proc_change')
         self.ppp_cb_boilerplate('on_hc_proc_exec')
         self.ppp_cb_boilerplate('on_hc_proc_vma_update')
+
+        self.vmas = []
+        self.current_proc = None
+        self.total_coverage = {} # procname: set of (modname, offset) tuples
+
+        @panda.cb_start_block_exec
+        def cov_sbe(cpu, tb):
+            if self.current_proc is None or self.current_proc['ignore'] or self.current_proc['name'] == "vpn":
+                return
+            pc = tb.pc
+            if name_mod_off := self.addr_to_mod_offset(pc):
+                name, mod, off = name_mod_off
+                #print(f"{name}: {mod}+{off}")
+                if name not in self.total_coverage:
+                    self.total_coverage[name] = set()
+                self.total_coverage[name].add((mod, off))
 
         @panda.cb_guest_hypercall
         def on_hypercall(cpu):
@@ -19,23 +35,21 @@ class Coverage(PyPlugin):
             arg = panda.arch.get_arg(cpu, 1)
                 
             if num == 590:
-                assert(not hasattr(self.pending_proc, 'name'))
                 try:
                     self.pending_proc['name' ] = panda.read_str(cpu, arg)
                 except ValueError:
                     self.pending_proc['name'] = "[error]"
             elif num == 591:
-                assert(not hasattr(self.pending_proc, 'pid'))
                 self.pending_proc['pid'] = arg
             elif num == 592:
-                assert(not hasattr(self.pending_proc, 'ppid'))
                 self.pending_proc['ppid'] = arg
             elif num == 593:
-                assert(not hasattr(self.pending_proc, 'create_time'))
                 self.pending_proc['create_time'] = arg
             elif num == 594:
-                assert(not hasattr(self.pending_proc, 'ignore'))
                 self.pending_proc['ignore'] = arg != 0
+
+                self.current_proc = copy.deepcopy(self.pending_proc)
+                self.pending_proc = {}
                 self.do_ppp('on_hc_proc_change', self.pending_proc)
 
             # Execve of new program (which is within an existing process)
@@ -46,6 +60,9 @@ class Coverage(PyPlugin):
                     name = panda.read_str(cpu, arg)
                 except ValueError:
                     name = "[error]"
+
+                self.pending_proc['name'] = name
+
                 self.do_ppp('on_hc_proc_exec', self.pending_proc)
 
             # VMA loop - build up a loop of VMAs
@@ -62,13 +79,15 @@ class Coverage(PyPlugin):
 
                 elif arg == 2 and self.in_vma_loop:
                     # Finished a VMA. Move pending into our list, reset pending
+                    if 'filename' not in self.pending_vma:
+                        self.pending_vma['filename'] = "[?]"
                     self.vmas.append(self.pending_vma)
                     self.pending_vma = {}
 
                 elif arg == 3 and self.in_vma_loop:
                     # Finished with all VMAs
                     self.in_vma_loop = False
-                    del self.pending_vma
+                    #del self.pending_vma
                     self.do_ppp('on_hc_proc_vma_update', self.vmas)
                 else:
                     print("ERROR: vma_loop_toggle with unexpected in_vma_loop", arg, self.in_vma_loop)
@@ -99,5 +118,22 @@ class Coverage(PyPlugin):
             # We did process the hypercall
             return True
 
+
     def do_ppp(self, name, *args):
-        print("coverage runs PPP", name, args)
+        #print("coverage runs PPP", name, args)
+        pass
+
+    def addr_to_mod_offset(self, addr):
+        # Walk through VMAs, find the one that contains this address
+        # returns (proc_name, module_name, offset)
+        for vma in self.vmas:
+            if addr >= vma['vma_start'] and addr < vma['vma_end']:
+                return (self.current_proc['name'], vma['filename'] if 'filename' else None, addr - vma['vma_start'])
+        return None
+    
+    def uninit(self):
+        # Write out coverage data
+        with open(self.outdir + "/coverage.log", "w") as f:
+            for proc, cov in self.total_coverage.items():
+                for mod, off in cov:
+                    f.write(f"{proc}: {mod}+{off}\n")
