cab2e49500e43246ff4bb044c7e907619b865e7b
Andrew Fasano <fasano@mit.edu>
2023-06-13 15:16:12 -0400

    Kernel args AI: try finding concrete values for identified vars

--- pyplugins/kernelargs.py
+++ pyplugins/kernelargs.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import glob
 import tiktoken
 import json
 from subprocess import check_output
@@ -16,31 +17,38 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Define standard locations for init scripts
 STANDARD_LOCATIONS = [
-        '/etc/rc.d/',
-        '/etc/init.d/',
-        '/etc/rc0.d/ to /etc/rc6.d/',
+        '/etc/rc*',
+        '/etc/init*',
         '/etc/systemd/system/',
-        '/usr/local/etc/rc.d/']
+        '/usr/local/etc/rc*/']
 
 STANDARD_FILES = [
     '/etc/rc.local'
 ]
 
 
+def find_files_in(directory):
+    scripts = []
+    if os.path.isfile(directory):
+        return [directory]
+    for item in os.listdir(directory):
+        full_path = os.path.join(directory, item)
+        if os.path.isfile(full_path):
+            scripts.append(full_path)
+        elif os.path.isdir(full_path):
+            scripts.extend(find_files_in(full_path))
+    return scripts
+
 # Function to find all init scripts in standard locations
 def find_init_scripts(root_dir):
     scripts = []
-    for location in STANDARD_LOCATIONS:
-        full_path = os.path.join(root_dir, '.' + location)
-        if os.path.isdir(full_path):
-            for script in os.listdir(full_path):
-                scripts.append(os.path.join(full_path, script))
+    for location in STANDARD_LOCATIONS+STANDARD_FILES:
+        path = os.path.abspath(os.path.join(root_dir, '.' + location)) # Note we allow *s in these
+        for full_path in glob.glob(path): # So we have to glob them
+            scripts.extend(find_files_in(full_path))
 
-    for location in STANDARD_FILES:
-        full_path = os.path.join(root_dir, location)
-        if os.path.isfile(full_path):
-            scripts.append(full_path)
-
+    # Check if file is a script
+    scripts = [s for s in scripts if b'shell script' in check_output(["file", s])]
     return scripts
 
 # Function to read a script
@@ -71,6 +79,19 @@ def calculate_cost(scripts):
     for (path, script) in scripts:
         ntoks += len(encoding.encode(path))
         ntoks += len(encoding.encode(script))
+
+    cost = ntoks * 0.002 / 1000
+
+    if cost < 0.5:
+        return True
+
+    print(f"Cost: {cost:.02f} USD")
+    res = input("Proceed? [y/n]")
+    return res == "y"
+
+def calculate_cost2(s):
+    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+    ntoks = len(encoding.encode(s))
 
     cost = ntoks * 0.002 / 1000
 
@@ -123,6 +144,28 @@ example_source_response = '[key, foo, mode, uid]'
 #        "mode": {'type': 'command', 'value': '/bin/get_mode -b'},
 #        "uid": {'type': 'dynamic', 'value': '${model}.${serial}'}
 #    }, sort_keys=False)
+
+
+ARG_PROMPT = 'You are a Linux system administrator assistant tasked with identifying potential kernel arguments to use when booting an arm or mips firmware image. You will be given a variable name and the output from grepping for file that reference the variable. You will provide a list of potential values the system may expect for the variable. You will build this list based off the grep output and any educated guesses you can make given the variable names and grep results. Values must be alphanmerical with dashes  and without spaces. Your response must start with ['
+
+example_arg = """
+File: /etc/run.d/10-foo
+3:if [ "$MYVAR" = "banana" ]; then
+8:if [ "$MYVAR" = "apple" ]; then
+121:    if [ "$MYVAR" = "caps" ]; then
+
+File: /usr/local/bin/helper.sh
+9: # Handle when MYVAR is "num" or scroll.
+10: # MYVAR is used for something
+11: # MYVAR: <string> holds data
+18: # MYVAR=<string> - variable
+19: MYVAR="pear"
+"""
+example_arg_response = '["banana", "apple", "caps", "num", "scroll", "pear", "orange"]'
+
+def make_arg_query(arg, grep_matches):
+    return f"Provide a list of potential values for the variable {arg} given the following logic that may be examining it:\n{grep_matches}"
+
             
 @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
 def completion_with_backoff(**kwargs):
@@ -190,12 +233,47 @@ def analyze_sourced_script(script_path, script):
     
     return decoded
 
+def find_arg_values(arg, grep_matches):
+    '''
+    E.g., arg='board' and grep_matches is the output from grepping the FS for 'board'
+    We want the AI to give us a list of sane values
+    '''
+
+    assert(len(grep_matches) < 100) # Sanity check
+    calculate_cost2(str(grep_matches)) # Ensure we don't break the bank
+
+    response = completion_with_backoff(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": ARG_PROMPT},
+
+            # Example prompt with response
+            {"role": "user", "content": make_arg_query('example', example_arg)},
+            {"role": "assistant", "content": example_arg_response},
+
+            # Actual prompt
+            {"role": "user", "content": make_arg_query(arg, grep_matches)} 
+        ],
+        temperature=0,
+    )
+
+    result = response.choices[0].message.content
+    try:
+        # Convert result into a python list from "[key, key2, ...]"
+        decoded = result.split("[")[1].split("]")[0].replace('"', '').split(", ")
+    except IndexError:
+        print(f"Failed to decode analysis of {arg}, {grep_matches}:\n{response}")
+        return None
+    
+    return decoded
+
 def analyze_fs(root_dir):
     DO_ONE, DO_TWO = (True, True)
 
     print("\nStage 1: analyze startup scripts")
     if DO_ONE:
         paths = find_init_scripts(root_dir)
+
         scripts = collect_scripts(paths, root_dir)
         if not calculate_cost(scripts):
             print("Cost not okay - abort")
@@ -216,14 +294,18 @@ def analyze_fs(root_dir):
             first_pass = json.load(f)
 
     # Second pass - examine all sourced files to find what they set
-    print("\nStage 2: analyze sourced files")
+    print("\nStage 2: analyze sourced/executed scripts")
     if DO_TWO:
         sourced = {}
         for (path, response) in first_pass.items():
-            if 'sources' not in response:
-                continue
+            sources = response['sources'] if 'sources' in response else []
 
-            for f in response['sources']:
+            for bin in response['binaries'] if 'binaries' in response else []:
+                # Check if they're shell scripts
+                if b'shell script' in check_output(["file", bin]):
+                    sources.append(bin)
+
+            for f in sources:
                 if f in sourced:
                     continue # Already analyzed
 
@@ -239,6 +321,7 @@ def analyze_fs(root_dir):
 
                 if response := analyze_sourced_script(rel_f, script):
                     sourced[f] = response
+
         # Cache to disk for debugging
         with open('sourced.json', 'w') as f:
             json.dump(sourced, f)
@@ -263,12 +346,41 @@ def analyze_fs(root_dir):
 
     # Finally, summarize results - what variables do not seem to get set
     print("\nStage 3: Summarize results")
+    unique_args = set()
     for (path, response) in first_pass.items():
         n_bins = len(response['binaries']) if 'binaries' in response else 0
         k_args = response['kernel_args'] if 'kernel_args' in response else []
+        unique_args.update([x for x in k_args if '/' not in x])
 
         if len(k_args):
             print(f"Startup script {path} runs {n_bins} binaries. Depends on potential kernel arguments: {k_args}")
+
+    print("\nUnique kernel arguments:")
+    print(unique_args)
+
+
+    # Now let's try finding default values for these arguments
+    potential_vals = {} # arg -> [val, val, ...]
+    for arg in unique_args:
+        # Find all files in the root directory that contain this arg
+        last_fname = None
+        output = check_output(["grep", "-rIHn", arg, root_dir])        
+        matches = {} # fname -> [line, line, ...]
+
+        for line in output.decode('utf-8').split('\n'):
+            fname = line.split(':')[0].replace(root_dir, '/')
+            line = line[len(fname)+1:]
+
+            if fname not in matches:
+                matches[fname] = []
+            matches[fname].append(line)
+
+        potential_vals[arg] = find_arg_values(arg, matches)
+
+    print("\nPotential values for kernel arguments:")
+    for (arg, vals) in potential_vals.items():
+        print(f"{arg}: {vals}")
+
 
 
 if __name__ == "__main__":
