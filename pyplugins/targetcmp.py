10fd6b9db2d8651c50c16a578064bdb36c7952be
Andrew Fasano <fasano@mit.edu>
2023-09-22 14:06:57 -0400

    targetcmp: conditionallly load

--- pyplugins/targetcmp.py
+++ pyplugins/targetcmp.py
@@ -1,7 +1,15 @@
 from pandare import PyPlugin
+import yaml
+from os.path import dirname
 
 class TargetCmp(PyPlugin):
     def __init__(self, panda):
         outdir = self.get_arg("outdir")
-        panda.load_plugin("callstack_instr", args={"stack_type": "asid"})
-        panda.load_plugin("targetcmp", args={"outdir": outdir, "target_str": "DYNVAL"})
+
+        with open(dirname(self.outdir) + "/config.yaml", "r") as f:
+            self.current_config = yaml.safe_load(f)
+
+        if 'DYNVAL' in self.current_config['base']['append']:
+            # Config specifies DYNVAL, so we'll dynamically analyze
+            panda.load_plugin("callstack_instr", args={"stack_type": "asid"})
+            panda.load_plugin("targetcmp", args={"outdir": outdir, "target_str": "DYNVAL"})
