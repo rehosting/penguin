5fe0686e570a7bc33e6d539d3eeee9e9f11701fe
Jacques Becker <jacques.becker20@gmail.com>
2023-12-15 12:15:29 -0500

    added support for collecting more argv and environment strings, and new process EUID and EGID

--- compiled_plugins/track_proc_hc/track_proc_hc.cpp
+++ compiled_plugins/track_proc_hc/track_proc_hc.cpp
@@ -16,59 +16,71 @@
 #include <vector>
 #include <tuple>
 #include <set>
-#include <map>
 #include <unordered_map>
 #include <byteswap.h> // bswap_32
 
 #define __STDC_FORMAT_MACROS
-#include "track_proc_hc.h"
 #include "panda/plugin.h"
+#include "panda/plugin_plugin.h"
 
 //#define TARGET_ARM  // XXX just for vscode highlight
 //#define DEBUG_PRINT
 
 extern "C" {
+  #include "track_proc_hc.h"
+  #include "track_proc_hc_ppp.h"
+
   PPP_PROT_REG_CB(on_hc_proc_change);
   PPP_PROT_REG_CB(on_hc_proc_exec);
   PPP_PROT_REG_CB(on_hc_proc_vma_update);
+
+  PPP_CB_BOILERPLATE(on_hc_proc_change);
+  PPP_CB_BOILERPLATE(on_hc_proc_exec);
+  PPP_CB_BOILERPLATE(on_hc_proc_vma_update);
+
 }
 
-PPP_CB_BOILERPLATE(on_hc_proc_change);
-PPP_CB_BOILERPLATE(on_hc_proc_exec);
-PPP_CB_BOILERPLATE(on_hc_proc_vma_update);
 
 bool in_vma_loop = false;
-proc_t pending_proc;
-vma_t* pending_vma;
-std::vector<vma_t*>* vmas = new std::vector<vma_t*>;
+struct proc_t pending_proc;
+struct vma_t* pending_vma;
+std::vector<struct vma_t*>* vmas = new std::vector<struct vma_t*>;
 
-void debug_print_proc(const char* msg, proc_t* p) {
+void debug_print_proc(const char* msg, struct proc_t* p) {
 #ifdef DEBUG_PRINT
   printf("[track_proc_hc] %s process %s pid %d\n", msg, p->comm, p->pid);
 #endif
 }
 
 bool before_hypercall(CPUState *cpu) {
-uint32_t num, arg = {0};
+uint32_t num, arg, arg2 = {0};
 #ifdef TARGET_ARM
   CPUArchState *env = static_cast<CPUArchState *>(cpu->env_ptr);
   num = env->regs[0];
   arg = env->regs[1];
-#elif defined(TARGET_MIPS)
+  arg2 = env->regs[2];
+//#elif defined(TARGET_MIPS) && defined(TARGET_WORDS_BIGENDIAN)   //MIPS
+#elif defined(TARGET_MIPS)  //MIPS
   CPUArchState *env = static_cast<CPUArchState *>(cpu->env_ptr);
-  // num in A0, arg in A1
   num = env->active_tc.gpr[4];
   arg = env->active_tc.gpr[5];
+  arg2 = env->active_tc.gpr[6];
 #else
   printf("Unsupported hypercall architecture\n");
   return false;
+#endif
+
+#ifdef DEBUG_PRINT
+  if (num != 0) {
+    //printf("BEFORE HYPERCALL - num: %x, arg: %x\n", num, arg);
+  }
 #endif
 
   //printf("Hypercall %d: 0x%x\n", num, arg);
   switch (num) {
     /////////// PROCESS SWITCH
     case 590: // Process switch starts with reporting  name
-      pending_proc = {0};
+      //pending_proc = {0};
 
       if (panda_virtual_memory_read(cpu, arg, (uint8_t*)&pending_proc.comm, sizeof(pending_proc.comm)) == -1) {
         strncpy(pending_proc.comm, "[error]", sizeof(pending_proc.comm));
@@ -98,8 +110,8 @@ uint32_t num, arg = {0};
 
     case 1595: { // parent create_time. XXX SKIP NUM
       pending_proc.parent_create_time = arg;
-      debug_print_proc("process switch", &pending_proc);
-      PPP_RUN_CB(on_hc_proc_change, (gpointer)&pending_proc, NULL);
+      //debug_print_proc("process switch", &pending_proc);
+      //PPP_RUN_CB(on_hc_proc_change, &pending_proc, NULL);
       break;
     }
 
@@ -115,24 +127,71 @@ uint32_t num, arg = {0};
       }
     break;
 
-    case 597: // argv[1] (first arument, maybe NULL)
-      if (arg == 0) {
-        pending_proc.arg1[0] = '\0';
-      } else if (panda_virtual_memory_read(cpu, arg, (uint8_t*)pending_proc.arg1, sizeof(pending_proc.arg1)) != 0) {
-        printf("ERROR: unable to read arg1 to %s on execve: GVA %x\n", pending_proc.comm, arg);
-      }
-      break;
+    case 597: //receives pointer to argv string (arg) and index (arg2)
+      #ifdef DEBUG_PRINT
+        printf("HC 597: received args %x, %d\n", arg, arg2);
+      #endif
 
-    case 598: // argv[2] (second arument, maybe NULL)
       if (arg == 0) {
-        pending_proc.arg2[0] = '\0';
-      } else if (panda_virtual_memory_read(cpu, arg, (uint8_t*)pending_proc.arg2, sizeof(pending_proc.arg2)) != 0) {
-        printf("ERROR: unable to read arg2 to %s %s on execve: GVA %x\n", pending_proc.comm, pending_proc.arg1, arg);
-      //} else {
-        //printf("Process %s gets arg[1,2] %s, %s\n", pending_proc.comm, pending_proc.arg1, pending_proc.arg2);
+        pending_proc.argv[arg2][0] = '\0';
+        printf("ERROR: argv[%d] is NULL on hypercall 597.\n", arg2);
+      } else if (panda_virtual_memory_read(cpu, arg, (uint8_t*)pending_proc.argv[arg2], sizeof(pending_proc.argv[arg2])) != 0) {
+        printf("ERROR: unable to read arg1 to proc %s argv[%d] on execve: GVA %x\n", pending_proc.comm, arg2, arg);
       }
+      #ifdef DEBUG_PRINT
+        printf("HC 597: read ptr %x -> %s\n", arg, pending_proc.argv[arg2]);
+      #endif
+    break;
+
+    case 598: //received argc
+      #ifdef DEBUG_PRINT
+        printf("HC 598: received arg %d\n", arg);
+      #endif
+
+      pending_proc.argc = arg;
+    break;
+
+    case 599: //receives pointer to envp string (arg) and index (arg2)
+      #ifdef DEBUG_PRINT
+        printf("HC 599: received args %x, %d\n", arg, arg2);
+      #endif
+
+      if (arg == 0) {
+        pending_proc.envp[arg2][0] = '\0';
+        printf("ERROR: envp[%d] is NULL on hypercall 599.\n", arg2);
+      } else if (panda_virtual_memory_read(cpu, arg, (uint8_t*)pending_proc.envp[arg2], sizeof(pending_proc.envp[arg2])) != 0) {
+        printf("ERROR: unable to read arg1 to proc %s argv[%d] on execve: GVA %x\n", pending_proc.comm, arg2, arg);
+      }
+      #ifdef DEBUG_PRINT
+        printf("HC 599: read ptr %x -> %s\n", arg, pending_proc.envp[arg2]);
+      #endif
+    break;
+
+    case 600: //receives envc
+      #ifdef DEBUG_PRINT
+        printf("HC 600: received arg %d\n", arg);
+      #endif
+
+      pending_proc.envc = arg;
+    break;
+
+
+    case 601: //receives euid
+      #ifdef DEBUG_PRINT
+        printf("HC 601: received euid: %u\n", arg);
+      #endif
+
+      pending_proc.euid = arg;
+    break;
+
+    case 602: //receives egid, and runs PPP callback
+      #ifdef DEBUG_PRINT
+        printf("HC 602: received egid: %u\n", arg);
+      #endif
+
+      pending_proc.egid = arg;
       debug_print_proc("process execve", &pending_proc);
-      PPP_RUN_CB(on_hc_proc_exec, (gpointer)&pending_proc, NULL);
+      PPP_RUN_CB(on_hc_proc_exec, cpu, &pending_proc, NULL);
     break;
 
     /// VMA LOOP. Build up a list of VMAs
@@ -147,23 +206,23 @@ uint32_t num, arg = {0};
         vmas->clear(); // Always clear VMAs for current proc on VMA update
         // XXX if someone (proc_map) tries to use VMAs while we're updating, they'll be invalid pointers!
         // to hack around this, we call the callback an extra time with an empty list
-        PPP_RUN_CB(on_hc_proc_vma_update, (gpointer)vmas, NULL);
+        //PPP_RUN_CB(on_hc_proc_vma_update, vmas, NULL);
 
-        pending_vma = new vma_t;
+        pending_vma = new struct vma_t;
 
       } else if (arg == 2 && in_vma_loop) {
         // Finished a VMA
         vmas->push_back(pending_vma); // Move current vma into list and allocate a new one
 
         // Allocate a new one
-        pending_vma = new vma_t;
+        pending_vma = new struct vma_t;
 
       } else if (arg == 3 && in_vma_loop) {
         // Finished with all VMAs. Note the last pending_vma we allocated is junk so we'll free it
         delete pending_vma;
         in_vma_loop = false;
         //debug_print_proc("vma update", &pending_proc);
-        PPP_RUN_CB(on_hc_proc_vma_update, (gpointer)vmas, NULL);
+        //PPP_RUN_CB(on_hc_proc_vma_update, vmas, NULL);
         vmas->clear();
 
       } else {
--- compiled_plugins/track_proc_hc/track_proc_hc.h
+++ compiled_plugins/track_proc_hc/track_proc_hc.h
@@ -1,38 +1,40 @@
-#ifndef __TRACKPROCHC_H__
-#include <glib.h>
-#include "panda/plugin.h"
+#ifndef TRACK_PROC_HC_H
+#define TRACK_PROC_HC_H
+//#include <glib.h>
+//#include "panda/plugin.h"
 
-// We match the qemu7 qpp api with two gpointers per callback
-PPP_CB_TYPEDEF(void, on_hc_proc_change, gpointer, gpointer);
-PPP_CB_TYPEDEF(void, on_hc_proc_exec, gpointer, gpointer);
-PPP_CB_TYPEDEF(void, on_hc_proc_vma_update, gpointer, gpointer);
 
-typedef struct {
+// BEGIN_PYPANDA_NEEDS_THIS -- do not delete this comment bc pypanda
+// api autogen needs it.  And don't put any compiler directives
+// between this and END_PYPANDA_NEEDS_THIS except includes of other
+// files in this directory that contain subsections like this one.
+
+struct vma_t{
   uint32_t vma_start;
   uint32_t vma_end;
   char filename[64];
 } vma_t;
 
-// assume contiguous regions
-typedef struct {
-  target_ulong start;
-  target_ulong end;
-} file_mapping;
 
-typedef struct {
+struct proc_t {
   uint32_t pid;
   uint32_t ppid;
+  uint32_t euid;
+  uint32_t egid;
   uint32_t create_time;
   uint32_t parent_create_time;
   char comm[64];
-  char arg1[64]; //argv[1] or NULL
-  char arg2[64]; //argv[2] or NULL
+  uint32_t argc;
+  uint32_t envc;
+  char argv[256][256];  //total bytes allowed for both argv and envp strings defined by ARG_MAX in include/uapi/linux/limits.h (131072 for linux 4.10)
+  char envp[128][512];  //environment variables can be long
   bool ignore;
-  std::map<std::string, file_mapping> mappings;
-
+  //std::vector<vma_t*>* vmas;
   //uint32_t prev_location;
   //uint32_t last_bb_start;
   //uint32_t last_bb_end;
-} proc_t;
+};
+
+// END_PYPANDA_NEEDS_THIS -- do not delete this comment!
 
 #endif
