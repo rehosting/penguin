74cd650060ab6cc6cc36f96e14234ed1bbdeb4fa
Andrew Fasano <fasano@mit.edu>
2023-12-27 14:28:13 -0500

    Add unit test: pseudofile_missing, pseudofile_ioctl, env_cmp, env_unset

---
+++ unittest/README.md
@@ -0,0 +1,32 @@
+# Penguin unit tests
+These directories contain stand-alone configs that we can realize without any rootfs to test failure detection and remediation. The `test.sh` script has logic for running each config
+for armel/mips/mipsel and checking if the expected failures are reported correctly
+
+Each config adds a shell script at `/init` that drives the guest to take some actions that should fail. Our dynamic analyses should detect these failures and propose mitigations.
+
+Run after building the container with:
+
+```sh
+docker build -t pandare/igloo:penguin ..
+./test.sh
+```
+
+# Working tests
+
+## EnvUnset
+* Shell script tries accessing `envvar` variable that's unset
+* Busybox instrumentation should detect this access
+* XXX: we also want to be able to detect direct accesses to /proc/cmdline but that's harder
+
+## EnvCmp
+* Guest compares the value of `envvar` to a constant `"target"`
+* The config sets envvar to DYNVALDYNVALDYNVAL (i.e., we had already identified it as a variable to solve for)
+* Expected results: `env_cmp.txt` in output directory contains `target`
+
+## PseudofileMissing
+* Guest attempts to access /dev/missing which doesn't exist
+* Expected results: `pseudofiles_failures.yaml` contains a key of `/dev/missing` with a nonzero count for a `stat`-like syscall
+
+## PseudofileIoctl
+* Guest issues an IOCTL on /dev/missing which has no ioctl model (using busybox hdparm utility)
+* Expected results: `pseudofiles_failures` contains a key of `/dev/missing` with an `ioctl` entry that has a key of `799` with a non-zero count.
---
+++ unittest/test.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+set -euo pipefail
+
+run_test() {
+  local arch=$1
+  local test_name=$2
+  local assertion=$3
+
+  if [ ! -d "$test_name" ]; then
+    echo "Test $test_name does not exist"
+    exit 1
+  fi
+
+  echo "Testing $test_name on architecture $arch..."
+  # If docker run fails, print log and bail
+  docker run --rm -it -v "$(pwd)":/tests pandare/igloo:penguin \
+    /tests/_in_container_run.sh "$arch" "$test_name" > log.txt || (tail log.txt && exit 1)
+
+  echo -n "$test_name: "
+  if $assertion; then
+    echo "PASS"
+  else
+    echo "FAIL"
+    echo
+    echo "Failure log:"
+    tail -n10 log.txt
+    echo
+    echo "console log:"
+    tail -n10 results/console.log
+    echo
+    echo
+  fi
+}
+
+assert_env_unset() {
+  grep -q "('envvar', None)" results/shell_env.csv
+}
+
+assert_env_cmp() {
+  grep -q 'target' results/env_cmp.txt
+}
+
+assert_pseudofiles_missing() {
+  python3 -c "import yaml; \
+              f = open('results/pseudofiles_failures.yaml'); \
+              assert yaml.safe_load(f)['/dev/missing'] is not None"
+}
+
+assert_pseudofiles_ioctl() {
+  python3 -c "import yaml; \
+              f = open('results/pseudofiles_failures.yaml'); \
+              data = yaml.safe_load(f); \
+              assert 'ioctl' in data['/dev/missing'] and \
+                      data['/dev/missing']['ioctl'][799]['count'] > 0"
+}
+
+mkdir -p results qcows
+
+archs=("armel" "mipsel" "mipseb")
+tests=("env_unset" "env_cmp" "pseudofile_missing" "pseudofile_ioctl")
+
+# We can run a single architecture or a single test.
+# For example:
+#   ./test.sh armel
+#   ./test.sh armel env_cmp
+
+if [ $# -eq 2 ]; then
+  archs=("$1")
+  tests=("$2")
+elif [ $# -eq 1 ]; then
+  archs=("$1")
+fi
+
+for arch in "${archs[@]}"; do
+  # For each test, check if it's in the list of tests to run
+  if [[ ! " ${tests[@]} " =~ " env_unset " ]]; then
+    echo "Skipping env_unset test for $arch"
+  else
+    run_test "$arch" "env_unset" assert_env_unset
+  fi
+
+  if [[ ! " ${tests[@]} " =~ " env_cmp " ]]; then
+    echo "Skipping env_cmp test for $arch"
+  else
+    run_test "$arch" "env_cmp" assert_env_cmp
+  fi
+
+  if [[ ! " ${tests[@]} " =~ " pseudofile_missing " ]]; then
+    echo "Skipping pseudofile_missing test for $arch"
+  else
+    run_test "$arch" "pseudofile_missing" assert_pseudofiles_missing
+  fi
+
+  if [[ ! " ${tests[@]} " =~ " pseudofile_ioctl " ]]; then
+    echo "Skipping pseudofile_ioctl test for $arch"
+  else
+    run_test "$arch" "pseudofile_ioctl" assert_pseudofiles_ioctl
+  fi
+done
