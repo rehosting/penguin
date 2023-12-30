2ca9d7c52ff301fc294304faf3e6d76f2e7504ff
Andrew Fasano <fasano@mit.edu>
2023-12-29 21:09:06 -0500

    Add search test

--- tests/test.sh
+++ tests/test.sh
@@ -133,6 +133,12 @@ for arch in "${archs[@]}"; do
   if [[ ! " ${tests[@]} " =~ " combined " ]]; then
     echo "Skipping combined test for $arch"
   else
-    run_test "$arch" "combined" assert_combined 40
+    run_test "$arch" "combined" assert_multirun_ranps 40
+  fi
+
+  if [[ ! " ${tests[@]} " =~ " search " ]]; then
+    echo "Skipping search test for $arch"
+  else
+    run_test "$arch" "search" assert_combined 40
   fi
 done
