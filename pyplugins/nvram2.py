511d30b46225c054545215361ab97fb39bc13357
Andrew Fasano <fasano@mit.edu>
2024-02-28 23:27:43 -0500

    nvram2: log keys and hit/miss as csv

--- pyplugins/nvram2.py
+++ pyplugins/nvram2.py
@@ -1,12 +1,14 @@
 from pandare import PyPlugin
 
-log = "nvram.log"
+log = "nvram.csv"
 
 class Nvram2(PyPlugin):
     def __init__(self, panda):
         self.outdir = self.get_arg("outdir")
         self.panda = panda
         self.ppp.Core.ppp_reg_cb('igloo_nvram_get', self.on_nvram_get)
+        with open(f'{self.outdir}/{log}', "w") as f:
+            f.write("key,hit\n")
 
 
     def on_nvram_get(self, cpu, key, hit):
@@ -15,5 +17,5 @@ class Nvram2(PyPlugin):
         key = key.split("/")[-1] # It's the full /igloo/libnvram_tmpfs/keyname path
 
         with open(f'{self.outdir}/{log}', 'a') as f:
-            f.write(f'{key} hit: {hit}\n') 
+            f.write(f'{key},{1 if hit else 0}')
         self.panda.arch.set_arg(cpu, 1, 0)
