e8647e08293317efdf9bc16953d810411218a210
Andrew Fasano <fasano@mit.edu>
2024-03-03 00:03:37 -0500

    netbinds: record time

--- pyplugins/netbinds.py
+++ pyplugins/netbinds.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import time
 from pandare import PyPlugin
 from os.path import join
 
@@ -10,6 +11,7 @@ class NetBinds(PyPlugin):
         self.outdir = self.get_arg("outdir")
         self.panda = panda
         self.seen_binds = set()
+        self.start_time = time.time()
 
         # The NetBinds.on_bind PPP callback happens on every bind.
         # Don't be confused by the vpn on_bind callback that happens
@@ -18,14 +20,16 @@ class NetBinds(PyPlugin):
         self.ppp_cb_boilerplate('on_bind')
 
         with open(join(self.outdir, BINDS_FILE), 'w') as f:
-            f.write(f"procname,ipvn,domain,guest_ip,guest_port\n")
+            f.write(f"procname,ipvn,domain,guest_ip,guest_port,time\n")
 
         self.ppp.Core.ppp_reg_cb('igloo_bind', self.on_bind)
 
     def on_bind(self, cpu, procname, is_ipv4, is_stream, port, sin_addr):
+        now = time.time()
         ipvn = 4 if is_ipv4 else 6
         sock_type = 'tcp' if is_stream else 'udp'
         is_le = self.panda.endianness == "little"
+        time_delta = now - self.start_time
 
         # Convert to little endian if necessary and ensure it's only 16 bits
         port = port & 0xffff
@@ -53,7 +57,7 @@ class NetBinds(PyPlugin):
 
         # Report the bind's info
         with open(join(self.outdir, BINDS_FILE), 'a') as f:
-            f.write(f"{procname},{ipvn},{sock_type},{ip},{port}\n")
+            f.write(f"{procname},{ipvn},{sock_type},{ip},{port},{time_delta:.3f}\n")
 
         # Trigger our callback
         self.ppp_run_cb('on_bind', sock_type, ipvn, ip, port, procname)
