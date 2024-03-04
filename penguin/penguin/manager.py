e82c99ba3e34bac12701c0e1a268aeaec466ffe4
Andrew Fasano <fasano@mit.edu>
2024-03-04 18:01:50 -0500

    Disable truncation timeout

--- penguin/penguin/manager.py
+++ penguin/penguin/manager.py
@@ -283,15 +283,16 @@ class Worker:
         if do_run:
             config_depth = self.config_manager.graph.get_config_depth(config)
             timeout = config.info.get('plugins', {}).get('core', {}).get('timeout', None)
-
-            if timeout is not None and not config.exclusive:
-                # We want to gradually increase timeout as we get deeper
-                # Exclusive nodes are important analyses and we don't want to rerun them. Let them have full time.
-                # Also if we truncate exclusive nodes we need to fix a bug where the truncation failure gets
-                # passed to the exclusive mitigation provider. It's a mess.
-                new_timeout = min(30 * config_depth, timeout)
-                truncated = timeout - new_timeout # How truncated were we?
-                timeout = new_timeout
+            # XXX: Disabled - premature optimization? It's nice when it works, but we might end up
+            # fixing low-priority failures that occur early instead of the important ones
+            #if timeout is not None and not config.exclusive:
+            #    # We want to gradually increase timeout as we get deeper
+            #    # Exclusive nodes are important analyses and we don't want to rerun them. Let them have full time.
+            #    # Also if we truncate exclusive nodes we need to fix a bug where the truncation failure gets
+            #    # passed to the exclusive mitigation provider. It's a mess.
+            #    new_timeout = min(30 * config_depth, timeout)
+            #    truncated = timeout - new_timeout # How truncated were we?
+            #    timeout = new_timeout
 
             for config_idx in range(n_config_tests):
                 out_dir = os.path.join(run_dir, "output" + (str(config_idx) if config_idx > 0 else ""))
