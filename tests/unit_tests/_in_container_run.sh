202a1343bb168e879e58e499c806485558f75afa
Andrew Fasano <fasano@mit.edu>
2024-03-28 14:30:39 -0400

    Refactor unit tests

--- tests/unit_tests/_in_container_run.sh
+++ tests/unit_tests/_in_container_run.sh
@@ -2,20 +2,21 @@
 set -eu
 
 # Run inside penguin container
-# USAGE: ./_in_container_run.sh [kernel_version] [arch] [test_dir_name]
+# USAGE: ./_in_container_run.sh [test_dir_name]
 # Test dir is relative to the /results/ directory
 
-KERNEL_VERSION=$1
+TESTDIR=$1
 ARCH=$2
-TESTDIR=$3
 
-# We need a "host file" for the hostfile test. Pre-position one in /tmp
-cat <<EOF > /tmp/init.bin
+RESULTS="${TESTDIR}/output"
+
+# We need a "host file" for the hostfile test. Pre-position one in /tmp/hostfile
+mkdir /tmp/hostfile
+cat <<EOF > /tmp/hostfile/init.bin
 #!/igloo/utils/sh
 echo custom bin runs;
 /igloo/utils/busybox ps;
 EOF
-
 
 # Share qcows between tests (only) if FS hashes match
 ln -s /tests/qcows "/tmp/qcows"
@@ -24,19 +25,18 @@ ln -s /tests/qcows "/tmp/qcows"
 # This lets us get around the fact that there's no arch to identify here and no real rootfs at all
 d=$(mktemp -d)
 touch "${d}/.foo"
-tar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2019-01-01' -c -C "${d}" . | gzip -n > /tmp/fs.tar.gz
-md5sum /tmp/fs.tar.gz
+tar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2019-01-01' -c -C "${d}" . | \
+  gzip -n > /tmp/fs.tar.gz
 rm -rf "$d"
 
-# Now make us an image in /tmp/empty
+# Now make us an image in /tmp/generated
 TARBALL="/tmp/fs.tar.gz"
-REPACK_DIR="/tmp/empty"
+REPACK_DIR="/tmp/generated"
 RESOURCE_DIR="/pkg/resources"
 STATIC_DIR="/igloo_static"
 
 WORK_DIR="${REPACK_DIR}/work"
 TARFILE="${REPACK_DIR}/fs.tar"
-FD="${WORK_DIR}/firmadyne"
 IGLOO="${WORK_DIR}/igloo"
 
 # Validate and prepare tarball
@@ -46,8 +46,7 @@ if [ ! -e "$TARBALL" ]; then
 fi
 
 # Create required directories
-mkdir -p "$WORK_DIR" "$FD" "$IGLOO/utils" "$IGLOO/keys"
-mkdir -p /tmp/artifacts
+mkdir -p "$WORK_DIR" "$IGLOO/utils" "$IGLOO/keys"
 
 # Extract fs.tar.gz -> work/fs.tar
 gunzip -c "$TARBALL" > "$TARFILE"
@@ -60,7 +59,7 @@ for t in "console" "libnvram" "utils.bin" "utils.source" "vpn"; do
     [ -d "$UTILS" ] || { echo "FATAL: Missing utilities directory $UTILS" >&2; exit 1; }
 
     for f in "$UTILS"/*.${ARCH} "$UTILS"/*.all; do
-        [ -e "$f" ] || { echo "WARN: No files matching pattern $f"; continue; }
+        [ -e "$f" ] || { continue; }
         fname=$(basename "$f")
         dest="${IGLOO}/utils/${fname%.*}"
         cp "$f" "$dest"
@@ -78,23 +77,8 @@ ln -s "/igloo/utils/busybox" "$IGLOO/utils/sleep"
 tar --append --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2019-01-01' -f "$TARFILE" -C "$WORK_DIR" .
 gzip "$TARFILE"
 
-fakeroot /pkg/scripts/makeImage.sh "${REPACK_DIR}/fs.tar.gz" /tmp/empty
-
-
-# Rewrite our config template to set the specified kernel version and arch values (in arch and kernel fields)
-cp /tests/$TESTDIR/config.yaml /tmp/config.yaml
-sed -i -e "s/@KERNEL_VERSION@/$KERNEL_VERSION/g" -e "s/@ARCH@/$ARCH/g" /tmp/config.yaml
-
-# If $ARCH isn't ARM we need to swap zImage for vmlinux
-if [ "$ARCH" != "armel" ]; then
-  sed -i "s/zImage/vmlinux/g" /tmp/config.yaml
-fi
-
-# Now render and run the config
-penguin --novsock --config /tmp/config.yaml /tests/results
-
-# QEMU inside Docker makes the shared dir only readable by root.
-# There are fmode and dmode options for virtfs but PANDA doesn't support them.
-if [ -d /tests/results/shared ]; then
-  chmod -R 777 /tests/results/shared
-fi
+fakeroot /pkg/scripts/makeImage.sh "${REPACK_DIR}/fs.tar.gz" /tmp/generated
+mkdir -p $RESULTS $TESTDIR
+chmod -R 777 $RESULTS $TESTDIR
+penguin --novsock --config $TESTDIR/config.yaml $RESULTS || (echo "Test failed"; chmod -R 777 $RESULTS; exit 1)
+chmod -R 777 $RESULTS $TESTDIR
---
+++ tests/unit_tests/test.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import re
+import subprocess
+import sys
+import tempfile
+import yaml
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+SCRIPT_PATH=Path(__file__).resolve().parent # Script's directory
+TEST_DIR=Path(f"{SCRIPT_PATH}/configs")
+BASE_CONFIG=Path(f"{SCRIPT_PATH}/base_config.yaml")
+DEFAULT_KERNELS = ["4.10",
+                    #"6.7"
+                   ]
+DEFAULT_ARCHS = ["armel", "mipsel", "mipseb"]
+
+def assert_generic(filepath, pattern):
+    '''
+    Does a file within the results directory contain a pattern?
+    '''
+    def assert_func(results_dir):
+        return re.search(pattern, (results_dir / Path(filepath)).read_text()) is not None
+    return assert_func
+
+def assert_yaml(filepath, key, subkeys=None, assertion=None):
+    def assert_func(results_dir):
+        data = yaml.safe_load((results_dir / Path(filepath)).open())
+        try:
+            data_key = data[key]
+            for subkey in (subkeys or []):
+                data_key = data_key[subkey]
+            if assertion:
+                return assertion(data_key)
+            return True
+        except KeyError:
+            return False
+    return assert_func
+
+class TestRunner:
+    def __init__(self, kernel_versions, archs, tests, checks):
+        self.kernel_versions = kernel_versions
+        self.archs = archs
+        self.tests = tests
+        self.checks = checks
+        self.qcows_dir = SCRIPT_PATH / Path("qcows")
+        self.qcows_dir.mkdir(exist_ok=True)
+
+    def run_test(self, kernel_version, arch, test_name, assertion):
+        # Create a scratch directory + results dir in it
+        with tempfile.TemporaryDirectory() as tmpdir:
+            self._generate_config(test_name, kernel_version, arch, tmpdir)
+            self._run_test(kernel_version, arch, test_name, tmpdir)
+
+            # Backup output from the run (not the full tmpdir, just output)
+            output_dir = SCRIPT_PATH / Path("results") / Path(f"{test_name}_{kernel_version}_{arch}")
+            # Replace the output directory if it already exists
+            if output_dir.exists():
+                subprocess.run(["rm", "-rf", output_dir])
+            output_dir.mkdir(exist_ok=True, parents=True)
+            subprocess.run(["mv", f"{tmpdir}/output", output_dir])
+            subprocess.run(["mv", f"{tmpdir}/config.yaml", output_dir])
+            subprocess.run(["mv", f"{tmpdir}/test_log.txt", output_dir])
+            if not self._check_results(test_name, output_dir / Path("output"), assertion):
+                logging.error(f"Test {test_name} failed on kernel version {kernel_version} with architecture {arch}")
+                return False
+        return True
+
+    def _generate_config(self, test_name, kernel_version, arch, tmpdir):
+        if not (test_path := (TEST_DIR / Path(test_name + ".yaml"))).is_file():
+            raise ValueError(f"Test {test_name} does not exist")
+
+        test_data = yaml.safe_load(test_path.open())
+
+        with open(BASE_CONFIG, "r") as f:
+            base_data = f.read()
+            base_data = base_data.replace("@KERNEL_VERSION@", kernel_version)
+            base_data = base_data.replace("@ARCH@", arch)
+            if arch == "armel":
+                base_data = base_data.replace("vmlinux", "zImage")
+        base_config = yaml.safe_load(base_data)
+
+        # For each key in test_data, update base_config - note it's a dict so we need to
+        # go through each key and update the base_config
+        for key in test_data:
+            if key in base_config:
+                base_config[key].update(test_data[key])
+            else:
+                base_config[key] = test_data[key]
+
+        # Write the updated base_config to a file in the tmpdir
+        with open(tmpdir / Path("config.yaml"), "w") as f:
+            yaml.dump(base_config, f)
+
+    def _run_test(self, kernel_version, arch, test_name, tmpdir):
+        logging.info(f"Testing {test_name} on kernel version {kernel_version} with architecture {arch}...")
+        try:
+            subprocess.run([
+                "docker", "run", "--rm", "-v", f"{SCRIPT_PATH}:/tests",
+                "-v", f"{tmpdir}:{tmpdir}",
+                "pandare/igloo:penguin", "/tests/_in_container_run.sh", tmpdir, arch
+            ],
+            check=True,
+            stdout=open(tmpdir / Path("test_log.txt"), "w"),
+            stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError:
+            logging.error("Docker run failed, showing last 30 lines from log:")
+            subprocess.run(["tail", "-n", "30", tmpdir / Path("test_log.txt")])
+            sys.exit(1)
+
+    def _check_results(self, test_name, outdir, assertion):
+        if assertion(outdir):
+            logging.info(f"{test_name}: PASS")
+            return True
+        else:
+            logging.info(f"{test_name}: FAIL: invalid results")
+            subprocess.run(["tail", "-n30", outdir.parent / Path("test_log.txt")])
+            subprocess.run(["tail", "-n30", outdir / Path("console.log")])
+        return False
+
+    def run_all(self):
+        num_fails = 0
+        for kernel_version in self.kernel_versions:
+            for arch in self.archs:
+                for test_name in self.tests:
+                    if not self.run_test(kernel_version, arch, test_name, self.checks[test_name]):
+                        num_fails += 1
+        if num_fails:
+            logging.error(f"{num_fails} tests failed")
+            sys.exit(1)
+
+def main():
+    tests_to_checks = {
+        "env_unset": assert_generic("shell_env.csv", "('envvar', None)"),
+        "env_cmp": assert_generic("env_cmp.txt", "target"),
+        "pseudofile_missing": assert_yaml("pseudofiles_failures.yaml", "/dev/missing"),
+        "pseudofile_ioctl": assert_yaml("pseudofiles_failures.yaml", "/dev/missing", ["ioctl", 799, "count"], lambda x: x > 0),
+        "hostfile": assert_generic("console.log", "\[bioset\]"),
+        "shared_dir": assert_generic("shared/from_guest.txt", "Hello from guest"),
+    }
+
+    parser = argparse.ArgumentParser(description="Run PENGUIN unit tests.")
+    parser.add_argument('--kernel-version', nargs='*', help=f'Kernel version(s) to test. Default: {", ".join(DEFAULT_KERNELS)}', default=DEFAULT_KERNELS)
+    parser.add_argument('--arch', nargs='*', help=f'Architecture(s) to test. Default: {", ".join(DEFAULT_ARCHS)}', default=DEFAULT_ARCHS)
+    parser.add_argument('--test', nargs='*', help=f'Specific test(s) to run. Default: {", ".join(list(tests_to_checks.keys()))}')
+    args = parser.parse_args()
+    kernel_versions = args.kernel_version
+    archs = args.arch
+
+    # Load all tests if none are specified, else filter the requested tests.
+    if args.test:
+        tests = [test for test in args.test if test in tests_to_checks]
+        if not tests:
+            raise ValueError("None of the specified tests are recognized.")
+        # Ensure that all requested tests are in the checks dictionary
+        for test in tests:
+            if test not in tests_to_checks:
+                raise ValueError(f"Test {test} is not recognized.")
+    else:
+        tests = list(tests_to_checks.keys())
+
+
+    runner = TestRunner(kernel_versions, archs, tests, tests_to_checks)
+    runner.run_all()
+
+if __name__ == "__main__":
+    main()
