084297a56c1723a9a5abab47f91065c07e4e4cbb
Andrew Fasano <fasano@mit.edu>
2024-04-01 13:52:49 -0400

    Init: if stracing add a sleep

--- penguin/resources/init.sh
+++ penguin/resources/init.sh
@@ -125,6 +125,7 @@ done
 if [ ! -z "${STRACE}" ]; then
   # Strace init in the background (to follow through the exec)
   /igloo/utils/sh -c "/igloo/utils/strace -f -p 1" &
+  /igloo/utils/sleep 1
   unset STRACE
 fi
 
