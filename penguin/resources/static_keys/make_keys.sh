c520a3641d7cbb306395e845809c32e35bcb7a26
Andrew Fasano <fasano@mit.edu>
2024-04-04 16:35:51 -0400

    key shims: add openssl 1024 and 4096 pem files

--- penguin/resources/static_keys/make_keys.sh
+++ penguin/resources/static_keys/make_keys.sh
@@ -5,6 +5,11 @@
 openssl genpkey -algorithm RSA -out ./rsa2048.pem -pkeyopt rsa_keygen_bits:2048
 openssl genpkey -algorithm RSA -out ./rsa4096.pem -pkeyopt rsa_keygen_bits:4096
 
+# TODO: missing commands to generate openssl keys?
+openssl req -new -x509 -days 3650 -key openssl_1024.key -out openssl_1024.pem
+openssl req -new -x509 -days 3650 -key openssl_2048.key -out openssl_2048.pem
+openssl req -new -x509 -days 3650 -key openssl_4096.key -out openssl_4096.pem
+
 openssl dsaparam -out ./dsaparam.pem 2048
 openssl gendsa -out ./dsa2048.pem ./dsaparam.pem
 
