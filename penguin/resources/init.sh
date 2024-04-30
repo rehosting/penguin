cc8662a1319ed9ce75eeb7b278c5d8df325b1d2b
Andrew Fasano <fasano@mit.edu>
2024-04-30 14:49:49 -0400

    LD_PRELOAD for root shell so interactive users can access nvram

--- penguin/resources/init.sh
+++ penguin/resources/init.sh
@@ -53,7 +53,7 @@ fi
 
 if [ ! -z "${ROOT_SHELL}" ]; then
   echo '[IGLOO INIT] Launching root shell';
-  ENV=/igloo/utils/igloo_profile /igloo/utils/console &
+  LD_PRELOAD=/igloo/lib_inject.so ENV=/igloo/utils/igloo_profile /igloo/utils/console &
   unset ROOT_SHELL
 fi
 
