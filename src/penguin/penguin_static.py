9dbdcebcd45f4bb332c48de73ba067a56fbece7c
alibobi <73675955+alibobi@users.noreply.github.com>
2024-07-19 16:29:43 -0400

    Fix(penguin_static): handle multiple symlinks when resolving init binaries
    Refactor symlink handling logic to walk from root of filesystem

--- src/penguin/penguin_static.py
+++ src/penguin/penguin_static.py
@@ -743,26 +743,53 @@ def _is_init_script(tarinfo, fs):
 
             # If it's a symlink, make sure the link target exists
             if tarinfo.issym():
-                link_target = tarinfo.linkname
+                link_target = tarinfo.name
 
-                # Now we need to make the symlink absolute
-                if not link_target.startswith("/"):
-                    # If it's not absolute, it's relative to the directory the symlink is in
-                    link_target = os.path.dirname(tarinfo.name) + "/" + link_target
-                    # Now simplify the path
-                    link_target = os.path.normpath(link_target)
+                sym_loop_ctr = 0
+                subpath = ""
+                while subpath != link_target:
+                    components = link_target.split(os.sep)
+                    for component in components:
+                        if component:
+                            subpath = os.path.join(subpath, component)
 
-                if not link_target.startswith("./"):
-                    link_target = "./" + os.path.normpath(link_target)
+                        try:
+                            filename = fs.getmember(subpath)
+                        except KeyError:
+                            logger.warning(
+                                f"Potential init '{tarinfo.name}' is a symlink to '{link_target}' which does not exist in the filesystem'"
+                            )
+                            return False
 
-                # Does link_target exist in fs?
-                try:
-                    fs.getmember(link_target)
-                except KeyError:
-                    logger.warning(
-                        f"Potential init '{tarinfo.name}' is a symlink to '{link_target}' which does not exist in the filesystem"
-                    )
-                    return False
+                        if filename.issym():
+                            newlink = filename.linkname
+
+                            if newlink.startswith("/"):
+                                link_target = os.path.normpath(
+                                    newlink + link_target[len(subpath):]
+                                )
+                            else:
+                                link_target = os.path.normpath(
+                                    os.path.dirname(subpath)
+                                    + "/"
+                                    + newlink
+                                    + link_target[len(subpath):]
+                                )
+
+                            if not link_target.startswith("./"):
+                                if link_target.startswith("/"):
+                                    link_target = "." + os.path.normpath(link_target)
+                                else:
+                                    link_target = "./" + os.path.normpath(link_target)
+                            subpath = ""
+                            break
+
+                    sym_loop_ctr += 1
+                    if sym_loop_ctr == 100:
+                        logger.warning(
+                            f"Potential init '{tarinfo.name}' is a symlink loop'"
+                        )
+                        return False
 
             # If we have init in the name, make sure it's not named .init (e.g., rc.d startup names)
             if "init" in name and name.endswith(".init"):
@@ -1237,7 +1264,7 @@ def add_nvram_meta(proj_dir, config, output_dir):
             if f is None:
                 continue
 
-            for (query, _) in queries:
+            for query, _ in queries:
                 # Check if query is in file
                 if query.encode() in f.read():
                     if key not in config["nvram"]:
