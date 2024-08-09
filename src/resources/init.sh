5f397d099f30ed26f3b70b74f4b2645913313de8
Benjamin Levy <153011444+be32826@users.noreply.github.com>
2024-08-09 14:12:07 -0400

    feat(shared_dir): core dump support (#212)

--- src/resources/init.sh
+++ src/resources/init.sh
@@ -37,17 +37,27 @@ if [ -e /igloo/utils/random_seed ]; then
   /igloo/utils/busybox cat /igloo/utils/random_seed > /dev/urandom
 fi
 
+if [ ! -z "${SHARED_DIR}" ]; then
+  unset SHARED_DIR
+  /igloo/utils/busybox mkdir /igloo/shared
+  echo '[IGLOO INIT] Mounting shared directory';
+  /igloo/utils/busybox mount -t 9p -o trans=virtio igloo_shared_dir /igloo/shared -oversion=9p2000.L
+
+  # Set up core dumps
+  #
+  # This has to come before setting up the root shell,
+  # because we want the `ulimit -c unlimited` to apply for programs run inside the root shell.
+  /igloo/utils/busybox mkdir -p /igloo/shared/core_dumps
+  # Make sure the underlying file is overwritten and not a hyperfs pseudofile at that path.
+  # One might want to make `/proc/sys/kernel/core_pattern` a pseudofile to prevent the guest from overwriting it.
+  /igloo/utils/busybox echo '/igloo/shared/core_dumps/core_%e.%p' > /igloo/pfs/real/proc/sys/kernel/core_pattern
+  ulimit -c unlimited
+fi
+
 if [ ! -z "${ROOT_SHELL}" ]; then
   echo '[IGLOO INIT] Launching root shell';
   LD_PRELOAD=lib_inject.so ENV=/igloo/utils/igloo_profile /igloo/utils/console &
   unset ROOT_SHELL
-fi
-
-if [ ! -z "${SHARED_DIR}" ]; then
-  /igloo/utils/busybox mkdir /igloo/shared
-  echo '[IGLOO INIT] Mounting shared directory';
-  /igloo/utils/busybox mount -t 9p -o trans=virtio igloo_shared_dir /igloo/shared -oversion=9p2000.L
-  unset SHARED_DIR
 fi
 
 if [ ! -z "${WWW}" ]; then
