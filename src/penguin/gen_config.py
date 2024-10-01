b2f52acc51f7d9b7d52463ee5a6dc2f0eb1f9ff9
Andrew Fasano <fasano@mit.edu>
2024-10-01 16:05:29 -0400

    refactor(penguin_static): generate patches for base config

--- src/penguin/gen_config.py
+++ src/penguin/gen_config.py
@@ -26,7 +26,7 @@ from .defaults import (
     static_dir,
 )
 from .penguin_config import dump_config
-from .penguin_static import generate_static_patches, find_env_options_and_init
+from .penguin_static import create_patches, find_env_options_and_init, log_potential_pseudofiles
 
 logger = getColoredLogger("penguin.gen_config")
 
@@ -40,7 +40,6 @@ def binary_filter(fsbase, name):
     return name.endswith((".so", ".ko")) or \
         ".so." in name or \
         name.endswith("busybox")
-
 
 def find_architecture(infile):
     with tempfile.TemporaryDirectory() as tmp:
@@ -105,8 +104,28 @@ def get_kernel_path(arch, end, static_dir):
     else:
         return static_dir + f"kernels/{DEFAULT_KERNEL}/" + "vmlinux" + f".{arch}{end}"
 
+def get_arch_info(arch, end):
+    '''
+    Our naming convention for architectures is a bit inconsistent. This function
+    returns the arch name, kernel suffix, and dylib directory for a given architecture.
+    '''
+    if arch == "aarch64":
+        # TODO: We should use a consistent name here. Perhaps aarch64eb?
+        arch_name = "aarch64"
+        arch_suffix = ".aarch64"
+        dylib_dir = join(static_dir, "dylibs", "arm64")
+    elif arch == "intel64":
+        arch_name = "intel64"
+        arch_suffix = ".x86_64"
+        dylib_dir = join(static_dir, "dylibs", "x86_64")
+    else:
+        arch_name = arch + end
+        arch_suffix = f".{arch}{end}"
+        dylib_dir = join(static_dir, "dylibs", arch + end)
+    return arch_name, arch_suffix, dylib_dir
 
-def make_config(fs, out, artifacts, timeout=None, auto_explore=False):
+
+def make_config(fs, out, artifacts_dir, timeout=None, auto_explore=False):
     logger.info(f"Generating new configuration for {fs}...")
     """
     Given a filesystem as a .tar.gz make a configuration
@@ -134,263 +153,174 @@ def make_config(fs, out, artifacts, timeout=None, auto_explore=False):
             f"Penguin should begin post extraction and be given a .tar.gz archive of a root fs, not {fs}"
         )
 
-    tmpdir = None
-    if artifacts is None:
+    if artifacts_dir is None:
         tmpdir = tempfile.TemporaryDirectory()
         output_dir = tmpdir.name
     else:
-        output_dir = Path(artifacts)
+        tmpdir = None
+        output_dir = Path(artifacts_dir)
         output_dir.mkdir(exist_ok=True)
 
-    # extract into output_dir/base/{image.qcow,fs.tar}
-    arch_identified = find_architecture(fs)
-    if arch_identified is None:
-        raise ValueError(f"Failed to determine architecture for {fs}")
-    arch, end = arch_end(arch_identified)
+    # Ensure created files (e.g., base/*.yaml, output/*.yaml), are
+    # readable/writable by everyone since non-container users will want to access them
+    os.umask(0o000)
 
-    if arch is None:
-        raise ValueError(f"Architecture {arch_identified} not supported ({arch}, {end})")
-
-    kernel = get_kernel_path(arch, end, static_dir)
-
+    # Create a base directory for filesystem archive and static analysis results
     base_dir = Path(output_dir, "base")
     base_dir.mkdir(exist_ok=True, parents=True)
     base_fs = Path(base_dir, "fs.tar.gz")
     shutil.copy(fs, base_fs)
 
-    data = {}
-    data["core"] = {
-        "arch": arch if arch in ["aarch64", "intel64"] else arch + end,
-        "kernel": kernel,
-        "fs": "./base/fs.tar.gz",
-        "root_shell": True,
-        "show_output": False,
-        "strace": False,
-        "ltrace": False,
-        "version": default_version,
-        "auto_patching": True,
-        "guest_cmd": False,
-    }
+    # TODO: refactor to have a single extract with patches into output_dir
+    arch_identified = find_architecture(fs)
+    if arch_identified is None:
+        raise ValueError(f"Failed to determine architecture for {fs}")
 
-    data["blocked_signals"] = []
-    data["netdevs"] = default_netdevs
+    arch, end = arch_end(arch_identified)
+    if arch is None:
+        raise ValueError(f"Architecture {arch_identified} not supported ({arch}, {end})")
+    arch_name, arch_suffix, dylib_dir = get_arch_info(arch, end)
 
-    data["env"] = {}
-    data["pseudofiles"] = default_pseudofiles
-    data["lib_inject"] = {"aliases": default_lib_aliases}
+    # Find init and create env.yaml in base directory
+    # TODO: refactor to have single extract with patches
+    best_init = find_env_options_and_init(output_dir, base_fs, f"{output_dir}/base/")
+    if best_init is None:
+        raise ValueError("Failed to find an init script")
 
-    data["static_files"] = {
-        "/igloo": {
-            "type": "dir",
-            "mode": 0o755,
+    config = {
+        "core": {
+            "arch": arch_name,
+            "kernel": get_kernel_path(arch, end, static_dir),
+            "fs": "./base/fs.tar.gz",
+            "root_shell": True,
+            "show_output": False,
+            "strace": False,
+            "ltrace": False,
+            "version": default_version,
         },
-        "/igloo/init": {
-            "type": "inline_file",
-            "contents": default_init_script,
-            "mode": 0o111,
+        "env": {
+            "igloo_init": best_init,
         },
-        "/igloo/utils/sh": {
-            "type": "symlink",
-            "target": "/igloo/utils/busybox",
+        "patches": [],
+        "blocked_signals": [],
+        "netdevs": [],
+        "lib_inject": {},
+        "pseudofiles": {},
+        "static_files": {
+            "/igloo": {
+                    "type": "dir",
+                    "mode": 0o755,
+                },
+                "/igloo/init": {
+                    "type": "inline_file",
+                    "contents": default_init_script,
+                    "mode": 0o111,
+                },
+                "/igloo/utils/sh": {
+                    "type": "symlink",
+                    "target": "/igloo/utils/busybox",
+                },
+                "/igloo/utils/sleep": {
+                    "type": "symlink",
+                    "target": "/igloo/utils/busybox",
+                },
+                # Pre-computed crypto keys
+                "/igloo/keys/*": {
+                    "type": "host_file",
+                    "mode": 0o444,
+                    "host_path": join(*[dirname(dirname(__file__)), "resources", "static_keys", "*"])
+                },
+                # Add ltrace prototype files. They go in /igloo/ltrace because /igloo is treated as ltrace's /usr/share, and the files are normally in /usr/share/ltrace.
+                "/igloo/ltrace/*": {
+                    "type": "host_file",
+                    "mode": 0o444,
+                    "host_path": join(*[static_dir, "ltrace", "*"]),
+                },
+
+                # Dynamic libraries
+                "/igloo/dylibs/*": {
+                    "type": "host_file",
+                    "mode": 0o755,
+                    "host_path": join(dylib_dir, "*"),
+                },
+
+                "/igloo/utils": {
+                    "type": "dir",
+                    "mode": 0o755,
+                },
+
+                # Add serial device in pseudofiles
+                # XXX: For mips we use major 4, minor 65. For arm we use major 204, minor 65.
+                # This is because arm uses ttyAMA (major 204) and mips uses ttyS (major 4).
+                "/igloo/serial": {
+                    "type": "dev",
+                    "devtype": "char",
+                    "major": 4 if "mips" in arch else 204,
+                    "minor": 65,
+                    "mode": 0o666,
+                }
         },
-        "/igloo/utils/sleep": {
-            "type": "symlink",
-            "target": "/igloo/utils/busybox",
-        },
+        "plugins": default_plugins,
+        "nvram": {},
     }
 
-    # Copy over the scripts that /igloo/init sources so that users can modify them
-    source_d_dir = join(*[dirname(dirname(__file__)), "resources", "source.d"])
-    for f in os.listdir(source_d_dir):
-        data["static_files"][f"/igloo/source.d/{f}"] = {
-            "type": "inline_file",
-            "contents": open(os.path.join(source_d_dir, f)).read(),
-            "mode": 0o755,
-        }
-
-    data["static_files"]["/igloo/keys/*"] = {
-        "type": "host_file",
-        "mode": 0o444,
-        "host_path": join(*[dirname(dirname(__file__)), "resources", "static_keys", "*"])
-    }
-
-    # Add ltrace prototype files.
-    #
-    # They to go in `/igloo/ltrace`, because `/igloo` is treated as ltrace's
-    # `/usr/share`, and the files are normally in `/usr/share/ltrace`.
-    data["static_files"][f"/igloo/ltrace/*"] = {
-        "type": "host_file",
-        "mode": 0o444,
-        "host_path": join(*[static_dir, "ltrace", "*"]),
-    }
-
-    arch_suffix = f".{arch}{end}"
-    dylib_dir = join(static_dir, "dylibs", arch + end)
-    if arch == "aarch64":
-        # TODO: We should use a consistent name here. Perhaps aarch64eb?
-        arch_suffix = ".aarch64"
-        dylib_dir = join(static_dir, "dylibs", "arm64")
-    elif arch == "intel64":
-        arch_suffix = ".x86_64"
-        dylib_dir = join(static_dir, "dylibs", "x86_64")
-
-	# Add dynamic libraries
-    data["static_files"][f"/igloo/dylibs/*"] = {
-        "type": "host_file",
-        "mode": 0o755,
-        "host_path": join(dylib_dir, "*"),
-    }
-
-    data["static_files"]["/igloo/utils"] = {
-        "type": "dir",
-        "mode": 0o755,
-    }
-
-    # Add executable binaries, can't use a full directory copy here because we rename things
+    # Always add our utilities into static files. Note that we can't currently use
+    #a full directory copy since we're doing some renaming.
+    # TODO: Refactor utility paths in container so we can just copy the whole directory
+    # for a given architecture.
     for util_dir in ["console", "libnvram", "utils.bin", "utils.source", "vpn"]:
         for f in os.listdir(join(static_dir, util_dir)):
             if f.endswith(arch_suffix) or f.endswith(".all"):
                 out_name = f.replace(arch_suffix, "").replace(".all", "")
-                data["static_files"][f"/igloo/utils/{out_name}"] = {
+                config["static_files"][f"/igloo/utils/{out_name}"] = {
                     "type": "host_file",
                     "host_path": f"/igloo_static/{util_dir}/{f}",
                     "mode": 0o755,
                 }
-
-	# Add serial device in pseudofiles
-    # XXX: For mips we use major 4, minor 65. For arm we use major 204, minor 65.
-    # This is because arm uses ttyAMA (major 204) and mips uses ttyS (major 4).
-    # so calling it ttyS1 is a bit of a misnomer, but we don't want to go patch the console
-    # binary to use a different path.
-    data["static_files"]["/igloo/serial"] = {
-        "type": "dev",
-        "devtype": "char",
-        "major": 4 if "mips" in data["core"]["arch"] else 204,
-        "minor": 65,
-        "mode": 0o666,
-    }
-
-    data["plugins"] = default_plugins
-
-    # Explicitly placing this at the end
-    data["nvram"] = {}
-
-    if auto_explore:
-        # If auto_explore, we'll enable extra plugins to generate coverage - unless we're told the VPN is disabled.
-        if "vpn" in data["plugins"] and data["plugins"]["vpn"].get("enabled", True):
-            # If we have VPN (which we will if we have vsock), turn on zap and nmap
-            for p in ["nmap"]:
-                if p in data["plugins"]:
-                    data["plugins"][p]["enabled"] = True
-
-        # Also disable root shell and set timeout to 5 minutes (unless told otherwise)
-        data["core"]["root_shell"] = False
-        data["plugins"]["core"]["timeout"] = timeout if timeout else 300
-    else:
-        # Interactive, let's enable root shell and fully delete some plugins
-        data["core"]["root_shell"] = True
-        for p in ["zap", "nmap" "coverage"]:
-            if p in data["plugins"]:
-                del data["plugins"][p]
-
-    # Make sure we have a base directory to store config
-    # and static results in.
-    if not os.path.isdir(os.path.join(output_dir, "base")):
-        os.makedirs(os.path.join(output_dir, "base"))
-
-    # If we create files (e.g., base/*.yaml, output/*.yaml), we want them to be
-    # readable/writable by everyone since non-container users will want to access them
-    os.umask(0o000)
-
-    init = find_env_options_and_init(output_dir, data, f"{output_dir}/base/")
-    if init is None:
-        raise ValueError("Failed to find an init script")
-    data["env"]["igloo_init"] = init
-
-    patch_names = generate_static_patches(
+    patch_names = create_patches(
         output_dir,
-        data,
+        config,
         f"{output_dir}/base/",
         f"{output_dir}/patches/")
-    
 
-    # TODO: Add patches by name into config['patches']
+    if len(patch_names):
+        # TODO: should this be ordered?
+        config["patches"] = list(patch_names)
 
-    if not auto_explore:
-        # We want to build this configuration for a single-shot rehost.
-        # We'll ensure it has an igloo_init set and we'll specify an ioctl model for all our pseudofiles in /dev
-        logger.info(
-            "Tailoring configuration for single-iteration: selecting init and configuring default catch-all ioctl models"
-        )
+    # TODO: in create_patches we do env.yaml, but here we do pseudofiles.yaml independently
+    log_potential_pseudofiles(
+        output_dir,
+        config,
+        f"{output_dir}/base/")
 
-        with open(f"{output_dir}/base/env.yaml", "r") as f:
-            static_env = yaml.safe_load(f)
-            if "igloo_init" in static_env and len(static_env["igloo_init"]) > 0:
-                data["env"]["igloo_init"] = static_env["igloo_init"][0]
-                logger.info(f"\tinit set to: {data['env']['igloo_init']}")
-                if len(static_env["igloo_init"]) > 1:
-                    logger.debug(
-                        f"\tOther options are: {', '.join(static_env['igloo_init'][1:])}"
-                    )
+    # For all identified pseudofiles, try adding them. This reliably causes kernel panics - are we running
+    # out of kernel memory or are we clobbering important things?
+    """
+    with open(f"{output_dir}/base/pseudofiles.yaml", 'r') as f:
+        static_pseudofiles = yaml.safe_load(f)
+        print(f"Static analysis identified {len(static_pseudofiles)} potential pseudofiles")
+        cnt = 0
+        for f in static_pseudofiles:
+            if f in data['pseudofiles']:
+                continue
 
-        # For all identified pseudofiles, try adding them. This reliably causes kernel panics - are we running
-        # out of kernel memory or are we clobbering important things?
-        """
-        with open(f"{output_dir}/base/pseudofiles.yaml", 'r') as f:
-            static_pseudofiles = yaml.safe_load(f)
-            print(f"Static analysis identified {len(static_pseudofiles)} potential pseudofiles")
-            cnt = 0
-            for f in static_pseudofiles:
-                if f in data['pseudofiles']:
-                    continue
+            if len(f.split("/")) == 3: # /proc/foo good, /proc/asdf/zoo less good
+                continue
+            cnt += 1
 
-                if len(f.split("/")) == 3: # /proc/foo good, /proc/asdf/zoo less good
-                    continue
-                cnt += 1
-
-                data['pseudofiles'][f] = {
-                    'read': {
-                        "model": "zero",
-                    },
-                    'write': {
-                        "model": "discard",
-                    }
+            data['pseudofiles'][f] = {
+                'read': {
+                    "model": "zero",
+                },
+                'write': {
+                    "model": "discard",
                 }
+            }
 
-                if f.startswith("/proc/mtd"):
-                    data['pseudo_files'][f]['name'] = "uboot." + f.split("/")[-1]
-            print(f"\tAdded {cnt} of them")
-        """
-
-        # Now we'll set a default ioctl model for all our pseudofiles in /dev
-        for dev in data["pseudofiles"]:
-            if dev.startswith("/dev/"):
-                data["pseudofiles"][dev]["ioctl"] = {
-                    "*": {
-                        "model": "return_const",
-                        "val": 0,
-                    }
-                }
-
-    else:
-        # Automated mode
-
-        # Turn on force_www -> it will probably help?
-        data["core"]["force_www"] = True
-
-        # Make sure we dont' have an igloo_init set
-        if "igloo_init" in data["env"]:
-            # Make sure we didn't set an igloo_init in our env if there are multiple potential values
-            with open(f"{output_dir}/base/env.yaml", "r") as f:
-                static_env = yaml.safe_load(f)
-                if "igloo_init" in static_env:
-                    if len(static_env["igloo_init"]) > 1:
-                        del data["env"]["igloo_init"]
-
-    # Data includes 'meta' field with hypotheses about files/devices
-    # Let's drop that. It's stored in base/{env,files}.yaml
-    if "meta" in data:
-        del data["meta"]
+            if f.startswith("/proc/mtd"):
+                data['pseudo_files'][f]['name'] = "uboot." + f.split("/")[-1]
+        print(f"\tAdded {cnt} of them")
+    """
 
     # Write config to both output and base directories. Disable flow style and width
     # so that our multi-line init script renders the way we want
@@ -402,7 +332,7 @@ def make_config(fs, out, artifacts, timeout=None, auto_explore=False):
             # (but do clobber the initial_config.yaml in base if it exists)
             logger.debug(f"Not overwriting existing config file: {outfile}")
             continue
-        dump_config(data, outfile)
+        dump_config(config, outfile)
 
     # Config is a path to output_dir/base/config.yaml
     if out:
@@ -421,7 +351,7 @@ def make_config(fs, out, artifacts, timeout=None, auto_explore=False):
     return final_out
 
 
-def fakeroot_gen_config(fs, out, artifacts, verbose):
+def fakeroot_gen_config(fs, out, artifacts_dir, verbose):
     o = Path(out)
     cmd = [
         "fakeroot",
@@ -431,7 +361,7 @@ def fakeroot_gen_config(fs, out, artifacts, verbose):
         "--out",
         str(o),
         "--artifacts",
-        artifacts,
+        artifacts_dir,
     ]
     if verbose:
         cmd.extend(["--verbose"])
