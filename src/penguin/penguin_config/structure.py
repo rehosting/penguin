e231cc2635d33071ca81d532d12a6da29f0d864c
Andrew Fasano <fasano@mit.edu>
2024-10-04 12:44:56 -0400

    feat(config validation): allow empty arch and kernel to allow for patches to specify this info

--- src/penguin/penguin_config/structure.py
+++ src/penguin/penguin_config/structure.py
@@ -61,13 +61,15 @@ class Core(BaseModel):
     arch: Annotated[
         Optional[Literal["armel", "aarch64", "mipsel", "mipseb", "mips64el", "mips64eb", "intel64"]],
         Field(
+            None,
             title="Architecture of guest",
             examples=["armel", "aarch64", "mipsel", "mipseb", "mips64el", "mips64eb", "intel64"],
         ),
     ]
     kernel: Annotated[
-        str,
+        Optional[Union[str]],
         Field(
+            None,
             title="Path to kernel image",
             examples=[
                 "/igloo_static/kernels/zImage.armel",
@@ -83,7 +85,7 @@ class Core(BaseModel):
         Union[str, None],
         Field(
             title="Project-relative path to filesystem tarball",
-            examples=["base/fs.tar"],
+            examples=["base/fs.tar.gz"],
         ),
     ]
     plugin_path: Annotated[
@@ -684,3 +686,349 @@ class Main(BaseModel):
     lib_inject: LibInject
     static_files: StaticFiles
     plugins: Annotated[dict[str, Plugin], Field(title="Plugins")]
+
+
+def _jsonify_dict(d):
+    """
+    Recursively walk a nested dict and stringify all the keys
+
+    This is required for jsonschema.validate() to succeed,
+    since JSON requires keys to be strings.
+    """
+    return {
+        str(k): _jsonify_dict(v) if isinstance(v, dict) else v for k, v in d.items()
+    }
+
+
+def _validate_config_schema(config):
+    """Validate config with Pydantic"""
+    Main(**config).model_dump()
+    jsonschema.validate(
+        instance=_jsonify_dict(config),
+        schema=Main.model_json_schema(),
+    )
+
+
+def _validate_config_options(config):
+    """Do custom checks for config option compatibility"""
+
+    import penguin
+
+    logger = penguin.getColoredLogger("config")
+
+    if config["core"].get("ltrace", False) and config["core"].get("arch", "").startswith(
+        "mips64"
+    ):
+        logger.error("ltrace does not support mips64")
+        sys.exit(1)
+
+
+def _validate_config(config):
+    _validate_config_schema(config)
+    _validate_config_options(config)
+
+
+def load_config(path, validate=True):
+    """Load penguin config from path"""
+    with open(path, "r") as f:
+        config = yaml.load(f, Loader=CoreLoader)
+    config_folder = Path(path).parent
+    # look for files called patch_*.yaml in the same directory as the config file
+    if config["core"].get("auto_patching", False) is True:
+        patch_files = list(config_folder.glob("patch_*.yaml"))
+        patches_dir = Path(config_folder, "patches")
+        if patches_dir.exists():
+            patch_files += list(patches_dir.glob("*.yaml"))
+        if patch_files:
+            if config.get("patches", None) is None:
+                config["patches"] = []
+            for patch_file in patch_files:
+                config["patches"].append(str(patch_file))
+    if config.get("patches", None) is not None:
+        patch_list = config["patches"]
+        for patch in patch_list:
+            # patches are loaded relative to the main config file
+            patch_relocated = Path(config_folder, patch)
+            config = patch_config(config, patch_relocated)
+    # when loading a patch we don't need a completely valid config
+    if validate:
+        _validate_config(config)
+        # Not required in schema as to allow for patches, but these really are required
+        if config["core"].get("arch", None) is None:
+            raise ValueError("No core.arch specified in config")
+        if config["core"].get("kernel", None) is None:
+            raise ValueError("No core.kernel specified in config")
+
+        if config["core"].get("fs", None) is None:
+            config["core"]["fs"] = "./base/empty_fs.tar.gz"
+            empty_fs_path = os.path.join(config_folder, "./base/empty_fs.tar.gz")
+            if not os.path.exists(empty_fs_path):
+                construct_empty_fs(empty_fs_path)
+    return config
+
+
+def dump_config(config, path):
+    """
+    Write penguin config to path
+    TODO: If we have a config that includes patches we should validate *after* patches.
+    For now we allow empty arch and kernel with patches filling them in later, but
+    validation doesn't check this
+    """
+    _validate_config(config)
+    with open(path, "w") as f:
+        f.write(
+            "# yaml-language-server: $schema=https://rehosti.ng/igloo/config_schema.yaml\n"
+        )
+        yaml.dump(config, f, sort_keys=False, default_flow_style=False, width=None)
+
+
+def hash_yaml_config(config: dict):
+    """
+    Given a config dict, generate a hash
+    """
+    target = config
+    if "meta" in config:
+        # We want to ignore the 'meta' field because it's an internal detail
+        config2 = deepcopy(config)
+        del config2["meta"]
+        target = config2
+    return hashlib.md5(str(target).encode()).hexdigest()
+
+
+def gen_docs_yaml_dump(x):
+    """
+    Convert `x` to YAML for use in generated docs.
+    We can't use `yaml.dump(x)` alone for this, becuase it appends "\n...\n".
+    """
+
+    s = yaml.dump(x)
+    term = "\n...\n"
+    s = s[: -len(term)] if s.endswith(term) else s
+    return s.strip()
+
+
+def gen_docs_type_name(t):
+    """Convert the Python type `t` to a string for use in generated docs."""
+
+    og = typing.get_origin(t)
+    args = typing.get_args(t)
+
+    if t == Star:
+        return '"*"'
+    elif og is Union:
+        return " or ".join(map(gen_docs_type_name, args))
+    elif og is Literal:
+        return " or ".join([f'`"{gen_docs_yaml_dump(a)}"`' for a in args])
+    elif og in (list, tuple):
+        return "list of " + gen_docs_type_name(args[0])
+    elif t is int:
+        return "integer"
+    elif t is str:
+        return "string"
+    elif t is bool:
+        return "boolean"
+    elif t is type(None):
+        return "null"
+    else:
+        raise ValueError(f"unknown type {t}")
+
+
+def gen_docs_field(path, docs_field, include_type=True):
+    """Generate docs for a single field of the config"""
+
+    assert (
+        docs_field.title is not None
+    ), f"config option {path} has no title: {docs_field}"
+    heading_hashes = "#" * (len(path) + 1)
+    include_docs = docs_field.default is not PydanticUndefined
+    path_prefix = f"`{'.'.join(path)}` " if path else ""
+    out = ""
+    out += f"{heading_hashes} {path_prefix}{docs_field.title}\n"
+    if include_type or include_docs:
+        out += "\n"
+        out += "|||\n"
+        out += "|-|-|\n"
+    if include_type:
+        out += f"|__Type__|{gen_docs_type_name(docs_field.type_)}|\n"
+    if include_docs:
+        out += f"|__Default__|`{gen_docs_yaml_dump(docs_field.default)}`|\n"
+    out += "\n"
+    if docs_field.description is not None:
+        out += docs_field.description + "\n"
+    out += "\n"
+    for e in docs_field.examples:
+        out += "```yaml\n"
+        out += gen_docs_yaml_dump(e) + "\n"
+        out += "```\n"
+        out += "\n"
+    return out
+
+
+@dataclasses.dataclass(frozen=True)
+class DocsField:
+    """Information about a field of the config, for generating docs"""
+
+    type_: type
+    title: Optional[str]
+    description: Optional[str]
+    default: Union[PydanticUndefinedType, Any]
+    examples: tuple[Any]
+
+    def from_type(type_: type) -> "DocsField":
+        """Create a `DocsField` from a Python type, which should probably inherit `BaseModel` or `RootModel`"""
+
+        # Change Optional[Optional[... Optional[T] ...]] to T
+        while (
+            typing.get_origin(type_) is Union
+            and len(typing.get_args(type_)) == 2
+            and typing.get_args(type_)[1] is type(None)
+        ):
+            type_ = typing.get_args(type_)[0]
+
+        if hasattr(type_, "model_config"):
+            # Inherits BaseModel or RootModel
+            title = type_.model_config["title"]
+            description = type_.__doc__
+            try:
+                default = type_.model_config["default"]
+            except KeyError:
+                default = PydanticUndefined
+            try:
+                examples = type_.model_config["json_schema_extra"]["examples"]
+            except (KeyError, TypeError):
+                examples = []
+        else:
+            # Doesn't inherit BaseModel or RootModel, so make all values empty
+            title = description = None
+            default = PydanticUndefined
+            examples = []
+        return DocsField(type_, title, description, default, examples)
+
+    def from_field(field) -> "DocsField":
+        """Create a `DocsField` from a Pydantic `Field`"""
+
+        return DocsField(
+            field.annotation,
+            field.title,
+            field.description,
+            field.default,
+            field.examples or [],
+        ).merge(DocsField.from_type(field.annotation))
+
+    def merge(self, other: "DocsField") -> "DocsField":
+        """Create a `DocsField` by combining two `DocsField`s, using the second to fill in gaps in the first"""
+        return DocsField(
+            self.type_,
+            self.title or other.title,
+            self.description or other.description,
+            other.default if self.default is PydanticUndefined else self.default,
+            self.examples + other.examples,
+        )
+
+
+def gen_docs(path=[], docs_field=DocsField.from_type(Main)):
+    """Generate docs for config format starting from the field at the given path"""
+
+    type_ = docs_field.type_
+    type_origin = typing.get_origin(type_)
+    type_args = typing.get_args(type_)
+
+    # The first type argument that inherits `BaseModel`.
+    # For example, if the type is `Optional[Env]`, this is `Env`
+    first_model_arg = next(
+        (a for a in type_args if hasattr(a, "model_fields")),
+        None,
+    )
+
+    is_model = hasattr(type_, "model_fields")  # Type inherits `BaseModel`
+    is_root_model = (
+        is_model and "root" in type_.model_fields
+    )  # Type inherits `RootModel`
+    out = ""
+
+    if is_root_model:
+        # The type inherits `RootModel`. It is a newtype or a union.
+
+        info = type_.model_fields["root"]
+        discrim_key = info.discriminator
+        ann = info.annotation
+        og = typing.get_origin(ann)
+        args = typing.get_args(ann)
+
+        if isinstance(discrim_key, str):
+            # The type is a tagged union
+            assert og is Union
+
+            # Generate docs for the union itself
+            out += gen_docs_field(path=path, docs_field=docs_field, include_type=False)
+
+            # Generate docs for each variant
+            for variant in args:
+                [discrim_val] = typing.get_args(
+                    variant.model_fields[discrim_key].annotation
+                )
+                out += gen_docs(
+                    path=path + [f"<{discrim_key}={discrim_val}>"],
+                    docs_field=DocsField.from_type(variant),
+                )
+        else:
+            # The type is a newtype.
+            # Collect its metadata and try again with the underlying type.
+            out += gen_docs(
+                path=path,
+                docs_field=DocsField.from_field(info).merge(docs_field),
+            )
+    elif is_model:
+        # The type inherits `BaseModel` but not `RootModel`
+
+        out += gen_docs_field(path=path, docs_field=docs_field, include_type=False)
+        for name, info in type_.model_fields.items():
+            out += gen_docs(
+                path=path + [name],
+                docs_field=DocsField.from_field(info),
+            )
+    elif type_origin is dict:
+        # The type is `dict[T, U]`.
+
+        # Generate docs for the dictionary itself
+        out += gen_docs_field(path=path, docs_field=docs_field, include_type=False)
+
+        # Generate docs for the value type
+        key_type, val_type = typing.get_args(type_)
+        key_type_str = gen_docs_type_name(key_type)
+        out += gen_docs(
+            path=path + [f"<{key_type_str}>"],
+            docs_field=DocsField.from_type(val_type),
+        )
+    elif type_origin is Union and first_model_arg is not None:
+        # The type is `Optional[T]`. Try again with just `T`.
+        out += gen_docs(
+            path=path,
+            docs_field=DocsField.from_type(first_model_arg),
+        )
+    else:
+        # The type does not inherit from `BaseModel` and it doesn't have an argument that does.
+        # It is probably a primative type, like `str` or `bool`.
+        # There is no more recursion to do for this field, so just generate docs for it.
+        out += gen_docs_field(path, docs_field)
+
+    return out
+
+
+if __name__ == "__main__":
+    import argparse
+
+    p = argparse.ArgumentParser()
+    sp = p.add_subparsers(required=True)
+
+    sp.add_parser(
+        "schema",
+        help="Write JSON schema for config to stdout",
+    ).set_defaults(func=lambda: print(yaml.dump(Main.model_json_schema(), indent=2)))
+
+    sp.add_parser(
+        "docs",
+        help="Write generated config docs to stdout",
+    ).set_defaults(func=lambda: print(gen_docs()))
+
+    p.parse_args().func()
