9b9710a98e607bf7f96dbb5f6e2765bf4f9c600f
Andrew Fasano <fasano@mit.edu>
2024-10-28 12:33:45 -0400

    fix(unittests): work without penguin patch command

--- tests/unit_tests/test_target/test.py
+++ tests/unit_tests/test_target/test.py
@@ -6,6 +6,8 @@ import sys
 from pathlib import Path
 import click
 import subprocess
+import yaml
+import tempfile
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,28 +20,6 @@ SCRIPT_PATH = Path(__file__).resolve().parent.parent  # Script's directory
 TEST_DIR = Path(__file__).resolve().parent
 
 proj_dir = Path(__file__).resolve().parent
-
-
-def penguin_patch(base, patch, image):
-    try:
-        subprocess.run(
-            [
-                os.path.dirname(os.path.dirname(SCRIPT_PATH)) + "/penguin",
-                "--image",
-                image,
-                "patch",
-                base,
-                patch,
-            ],
-            check=True,
-            # stdout=open(proj_dir / Path("test_log.txt"), "w"),
-            # stderr=subprocess.STDOUT,
-        )
-    except subprocess.CalledProcessError:
-        logger.error("Penguin patch failed, showing last 50 lines from log:")
-        subprocess.run(["tail", "-n", "50", proj_dir / Path("test_log.txt")])
-        sys.exit(1)
-
 
 def penguin_run(config, image):
     try:
@@ -65,10 +45,17 @@ def run_test(kernel, arch, image):
     subprocess.check_output(f"tar -czvf {TEST_DIR}/empty_fs.tar.gz -T /dev/null", shell=True)
     base_config = str(Path(TEST_DIR, "base_config.yaml"))
     new_config = str(Path(TEST_DIR, "config.yaml"))
-    shutil.copyfile(base_config, new_config)
     os.makedirs(str(Path(TEST_DIR, "base")), exist_ok=True)
-    arch_patch = str(Path(TEST_DIR, f"patches/arches/{arch}-{kernel}.yaml"))
-    penguin_patch(new_config, arch_patch, image)
+
+    with open(base_config, "r") as file:
+        base_config = yaml.safe_load(file)
+
+    base_config["patches"].append(f"patches/arches/{arch}-{kernel}.yaml")
+
+    with open(new_config, "w") as file:
+        yaml.dump(base_config, file)
+
+    logger.info("Created new config file at " + new_config)
     penguin_run(new_config, image)
     logger.info("Test completed")
 
