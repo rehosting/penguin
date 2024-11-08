0b4681cee941107f4f69888d1a0db4e4b7344075
Zak Estrada <zachary.estrada@ll.mit.edu>
2024-11-07 22:53:37 -0500

    move FICD out of the health into its own plugin

--- pyplugins/health.py
+++ pyplugins/health.py
@@ -2,69 +2,6 @@ import time
 from os import path
 from pandare import PyPlugin
 from penguin import getColoredLogger
-import Levenshtein as lv
-
-
-class FICD:
-    """
-    FICD metric based on Pandawan, see https://github.com/BUseclab/Pandawan/blob/main/plugins/pandawan/ficd.py
-    The goal here is to produce a faithful representation of that metric in PENGUIN
-
-    "To this end, FICD considers that a firmware image reached If in if no previously unseen (i.e., unique) tasks are launched within tf seconds. We refer to tf as the time frame parameter"
-    "In our re-hosting experiments we use three (Py)PANDA plugins (coverage, syscalls_logger, and SyscallToKmodTracer) along with the FICD plugin, which results in the optimal tf = 220sec and tf = 300sec"
-    """
-    def __init__(self, start_time, outdir, logger, time_frame=300):
-        self.time_frame = time_frame
-        self.init_time = start_time
-        self.unique_proc_times = {}
-        self.seen_procs = set()
-        self.prev_time = time.time()
-        self.if_reached = False
-        self.outfile = path.join(outdir, "ficd.txt")
-        self.last_proc_time = 0
-        self.logger = logger
-
-        # FICD isn't reached until it is
-        with open(self.outfile, "w") as f:
-            f.write("0")
-
-    def on_exec(self, newproc):
-        self.prev_time = time.time()
-
-        for proc in self.seen_procs:
-            lev_ratio = lv.ratio(proc, newproc)
-            if lev_ratio >= 0.5:
-                self.unique_proc_times[newproc] = [round(self.prev_time - self.init_time, 2), "Not Unique"]
-                measured_tf = self.prev_time - self.last_proc_time
-                if measured_tf > self.time_frame:
-                    self.if_reached = True
-                    self.logger.info(f"FICD If reached on exec occuring {self.prev_time - self.init_time} after start")
-                return
-
-        if self.if_reached:
-            self.logger.warning(f"Warning! FICD If reached but new exec {newproc} was seen at time {self.prev_time - self.init_time}. System is likely being exercised.")
-        self.last_proc_time = self.prev_time
-        self.seen_procs.add(newproc)
-        self.unique_proc_times[newproc] = [round(self.prev_time - self.init_time, 2), "Unique"]
-
-    def done(self):
-        """
-        TODO: Since we aren't killing emulation after "If" is reached, it is possible that we stick
-        around long enough for a new unique process to
-        be created. Which would violate the underlying assumption about the
-        latching nature of FICD. Might be worth tracking to see if this ever happens.
-        """
-        prev_time = time.time()
-        measured_tf = prev_time - self.last_proc_time
-        if measured_tf > self.time_frame:
-            with open(self.outfile, "w") as f:
-                f.write("1")
-        with open(path.join(path.dirname(self.outfile), "ficd_times.txt"), "w") as f:
-            f.write(f"Measured TF: {measured_tf}\n")
-            f.write(f"Last Proc Start Time: {self.last_proc_time - self.init_time}\n")
-            f.write(f"Total Execution Time: {prev_time - self.init_time}\n")
-            f.write(f"Time Executed past TF: {measured_tf - self.time_frame}\n")
-
 
 class Health(PyPlugin):
     def __init__(self, panda):
@@ -73,12 +10,6 @@ class Health(PyPlugin):
         self.start_time = time.time()
         self.exiting = False
         self.logger = getColoredLogger("plugins.health")
-        if self.get_arg_bool("verbose"):
-            self.logger.setLevel("DEBUG")
-        if self.get_arg_bool("ficd"):
-            self.ficd = FICD(self.start_time, self.outdir, self.logger)
-        else:
-            self.ficd = None
 
         # XXX no longer used to track time
         self.events = {  # Class: [(time, score)]
@@ -127,8 +58,6 @@ class Health(PyPlugin):
             try:
                 argv_buf = panda.virtual_memory_read(cpu, argv_ptr, 96, fmt="ptrlist")
             except ValueError:
-                if self.ficd:
-                    self.ficd.on_exec(fname)
                 return
 
             # Read each argument pointer into argv list
@@ -143,9 +72,6 @@ class Health(PyPlugin):
                 except ValueError:
                     argv.append(f"(error: 0x{ptr:x})")
                     nullable_argv.append(None)
-            if self.ficd:
-                self.ficd.on_exec(fname + " " + " ".join(argv))
-
             try:
                 self.ppp_run_cb("igloo_exec", cpu, fname, nullable_argv)
             except Exception as e:
@@ -237,5 +163,3 @@ class Health(PyPlugin):
         with open(f"{self.outdir}/health_procs_with_args.txt", "w") as f:
             for proc in sorted(self.procs_args):
                 f.write(f"{proc}\n")
-        if self.ficd:
-            self.ficd.done()
