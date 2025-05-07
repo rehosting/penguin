051511d1576b0f8e8d7b11cd517cc1d702d757c6
Luke Craig <lacraig3@gmail.com>
2025-05-07 16:37:35 -0400

    rw: update with hypermem

--- pyplugins/analysis/rw.py
+++ pyplugins/analysis/rw.py
@@ -8,17 +8,16 @@ class RWLog(PyPlugin):
         self.panda = panda
         self.outdir = self.get_arg("outdir")
         self.DB = plugins.DB
-        panda.hsyscall("on_sys_write_return")(self.write)
-        panda.hsyscall("on_sys_read_return")(self.read)
+        self.hyp = plugins.hypermem
+        panda.hsyscall("on_sys_write_return")(self.hyp.wrap(self.write))
+        panda.hsyscall("on_sys_read_return")(self.hyp.wrap(self.read))
 
     def write(self, cpu, proto, syscall, hook, fd, buf, count):
-        try:
-            s = self.panda.read_str(cpu, buf, max_length=count)
-        except ValueError:
-            s = "error"
+        s = yield from self.hyp.read_str(buf)
 
         signed_fd = int(self.panda.ffi.cast("target_long", fd))
-        fname = self.panda.get_file_name(cpu, fd) or b"?"
+        fname = yield from self.hyp.get_fd_name(fd) or b"?"
+        fname = self.panda.read_file_name(cpu, fd) or b"?"
         self.DB.add_event(
             Write(
                 fd=signed_fd,
@@ -28,11 +27,9 @@ class RWLog(PyPlugin):
         )
 
     def read(self, cpu, proto, syscall, hook, fd, buf, count):
-        try:
-            s = self.panda.read_str(cpu, buf, max_length=count)
-        except ValueError:
-            s = "error"
-        fname = self.panda.get_file_name(cpu, fd) or b"?"
+        s = yield from self.hyp.read_str(buf)
+        signed_fd = int(self.panda.ffi.cast("target_long", fd))
+        fname = yield from self.hyp.get_fd_name(fd) or b"?"
         # Get name of FD, if it's valid
         signed_fd = int(self.panda.ffi.cast("target_long", fd))
         self.DB.add_event(
