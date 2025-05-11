fb5ee4144168e2c49c9aca3e86d2b95568809131
Luke Craig <lacraig3@gmail.com>
2025-05-10 21:44:22 -0400

    consts: cdef everything in includes folder

--- pyplugins/hyper/consts.py
+++ pyplugins/hyper/consts.py
@@ -1,5 +1,6 @@
 from cffi import FFI
 from penguin.defaults import DEFAULT_KERNEL
+from glob import glob
 
 ffi =FFI()
 ffi.cdef("typedef uint64_t __le64;")
@@ -9,8 +10,8 @@ def cdef_file(filename):
     with open(filename) as f:
         return ffi.cdef(f.read())
 
-cdef_file(f"/igloo_static/kernels/{DEFAULT_KERNEL}/portal_types.h")
-cdef_file(f"/igloo_static/kernels/{DEFAULT_KERNEL}/igloo_hypercall_consts.h")
+for f in glob(f"/igloo_static/kernels/{DEFAULT_KERNEL}/includes/*.h"):
+    cdef_file(f)
 
 c = ffi.dlopen('c')
 
