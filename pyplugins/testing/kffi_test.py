26e4f9041b7269ae4f1553bd449320308c8a59cc
Luke Craig <lacraig3@gmail.com>
2025-05-14 13:36:24 -0400

    add kffi test

---
+++ pyplugins/testing/kffi_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from pandare2 import PyPlugin
+from penguin import getColoredLogger, plugins
+
+kffi = plugins.kffi
+portal = plugins.portal
+
+class KFFITest(PyPlugin):
+    def __init__(self, panda):
+        self.panda = panda
+        self.outdir = self.get_arg("outdir")
+        self.logger = getColoredLogger("plugins.kffi_test")
+        if self.get_arg_bool("verbose"):
+            self.logger.setLevel("DEBUG")
+        self.panda.hsyscall(
+            "on_sys_ioctl_return", arg_filter=[0x14, 0x15, 0x16])(self.kffi)
+
+    @plugins.portal.wrap
+    def kffi(self, cpu, proto, syscall, hook, fd, op, arg):
+        args = [3, 8, 9, 0x1338c0de, 12, 13, 14, 15]
+        val = yield from kffi.call_kernel_function("igloo_test_function", *args)
+        assert val == sum(args), f"Expected {sum(args)}, got {val}, r/w failed"
+
+        buf = yield from kffi.kmalloc(100)
+        level = b"\x01\x03"
+        yield from portal.write_bytes(buf, level + b"test printk %d %d %d %d\x00")
+
+        yield from kffi.call_kernel_function("igloo_printk", buf, 1, 2, 3, 4)
+
+        yield from kffi.kfree(buf)
+
+        
