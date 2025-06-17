9905edb5fa476a61ddfc16436b4c73333341e2f5
Luke Craig <lacraig3@gmail.com>
2025-06-17 16:24:01 -0400

    drop penguinanalysis from pyplugins

--- pyplugins/analysis/env.py
+++ pyplugins/analysis/env.py
@@ -1,20 +1,7 @@
 import itertools
 import re
-from copy import deepcopy
-from os.path import isfile
 from os.path import join as pjoin
-from typing import List
-
-from penguin import plugins, Plugin
-
-try:
-    from penguin import yaml
-    from penguin.analyses import PenguinAnalysis
-    from penguin.graphs import Configuration, Failure, Mitigation
-except ImportError:
-    # We can still run as a PyPlugin, but we can't do post-run analysis
-    PenguinAnalysis = object
-    import yaml
+from penguin import plugins, Plugin, yaml
 
 ENV_MAGIC_VAL = "DYNVALDYNVALDYNVAL"  # We want this to be longer than the other strings we might compare to
 # If we change this we also need to change the regex below
@@ -203,7 +190,7 @@ class EnvTracker(Plugin):
         return True
 
 
-class TargetCmp(PyPlugin):
+class TargetCmp(Plugin):
     """
     Our 2nd pyplugin for environemnt tracking.
 
@@ -217,17 +204,16 @@ class TargetCmp(PyPlugin):
     there's no magic value in our env
     """
 
-    def __init__(self, panda):
+    def __init__(self):
         self.target_key = self._get_target_str_in_config(self.get_arg("conf"))
         if not self.target_key:
             return
+        panda = self.panda
 
         # XXX: We need to disable tb_chaining to detect more comparisons. Unfortunately
         # this hurts performance, but without it we definitely miss some comparisons
         # in targetcmp/callwitharg/callstack_instr.
         panda.disable_tb_chaining()
-
-        self.logger = getColoredLogger("plugins.TargetCmp")
         if self.get_arg_bool("verbose"):
             self.logger.setLevel("DEBUG")
 
@@ -335,270 +321,3 @@ class TargetCmp(PyPlugin):
                 f"Multiple matches for ENV_MAGIC_VAL ({ENV_MAGIC_VAL}) in config: {matches}"
             )
         return matches[0]
-
-
-class EnvTrackerAnalysis(PenguinAnalysis):
-    ANALYSIS_TYPE = "env"
-    VERSION = "1.0.0"
-
-    DEFAULT_VALUES = [
-        "1",
-        "0",
-        "no",
-        "0.0.0.0",
-        "00:00:00:00:00:00",
-    ]
-
-    def parse_failures(self, output_dir) -> List[Failure]:
-        """
-        Parse failures from env_missing.yaml for unset env variables.
-        Also if we have shell_env.csv, look in there for unset variables too.
-
-        If we have a DYNVALDYNVALDYNVAL in our env, it should be from an exclusive
-        config so we're the only plugin that can analyze. In that case, it's
-        the only failure we should consider (since we must address before we
-        move on.
-        """
-
-        with open(pjoin(output_dir, "core_config.yaml")) as f:
-            config = yaml.safe_load(f)
-
-        # Check for our magic variable in boot env, uboot env, and pseudofile read models
-        magic_is_set = any(x == ENV_MAGIC_VAL for x in config["env"].values()) or any(
-            x == ENV_MAGIC_VAL for x in config.get("uboot_env", {}).values()
-        )
-
-        if not magic_is_set:
-            for path, file_details in config.get("pseudofiles", {}).items():
-                if (
-                    file_details.get("read", None)
-                    and file_details["read"].get("model", None) == "const_buf"
-                ):
-                    if (
-                        file_details["read"].get("buf", None)
-                        and file_details["read"].get("buf") == ENV_MAGIC_VAL
-                    ):
-                        magic_is_set = True
-                        break
-
-        if magic_is_set:
-            target_var = [k for k, v in config["env"].items() if v == ENV_MAGIC_VAL][0]
-
-            dyn_vals = set()
-            if isfile(pjoin(output_dir, cmp_output)):
-                # Looks like we were running with ENV_MAGIC_VAL. Let's record these results too
-                # We don't know the name of the env_var though. Hmm.
-                with open(pjoin(output_dir, cmp_output)) as f:
-                    for line in f.readlines():
-                        line = line.strip()
-                        # Regex to check if it's a valid environment variable
-                        if re.match(r"^[A-Za-z_0-9][A-Za-z0-9-_\./]*$", line):
-                            dyn_vals.add(line)
-                        else:
-                            print(
-                                "Ignoring cpp-discovered dynval as it doesn't match regex:",
-                                line,
-                            )
-
-            if len(dyn_vals) > 10:
-                print(
-                    f"Warning, found {len(dyn_vals)} dynamic values for {target_var}. This is a lot! Filtering to first 3"
-                )
-                sorted_vals = sorted(
-                    dyn_vals, key=lambda x: len(x)
-                )  # Sort is so we'll generate the same ones in future analyses
-                dyn_vals = sorted_vals[:3]
-
-            # print(f"Found {len(dyn_vals)} dynamic values for {target_var}: {dyn_vals}")
-
-            # We found things dynamically. Cool. This is a single failure with details for these values
-            if len(dyn_vals) > 0:
-                return [
-                    Failure(
-                        f"dynval_{target_var}",
-                        self.ANALYSIS_TYPE,
-                        {"var": target_var, "values": dyn_vals, "source": "dynamic"},
-                        patch_name=target_var
-                    )
-                ]
-            else:
-                # We found nothing. Time to give up on this. Probably an uninteresting variable
-                return []
-
-        with open(pjoin(output_dir, missing_output)) as f:
-            env_accesses = yaml.safe_load(f)
-            assert isinstance(
-                env_accesses, list
-            ), f"Expected list of env accesses, got {env_accesses}"
-
-        if isfile(pjoin(output_dir, shell_env_output)):
-            # Shell plugin may have detected some env accesses too. Let's take a look
-            seen_envs = {}  # name -> values
-            with open(pjoin(output_dir, shell_env_output)) as f:
-                for line in f.readlines()[1:]:  # Skip header
-                    # Recover the env list from the line.
-                    # This storage format is kinda gross
-                    idx = line.index(",[")
-                    envs = line[idx + 1:].strip()
-                    if not len(envs):
-                        continue
-                    env_tuples = eval(envs)  # XXX sad eval
-                    if not len(env_tuples):
-                        continue
-                    for name, val in env_tuples:
-                        if name not in seen_envs:
-                            seen_envs[name] = set()
-                        seen_envs[name].add(val)
-
-            # Now look through the env names we've seen. Try finding any names that were always None.
-            # Add these to our env_accesses list if they're not already there
-            for k, v in seen_envs.items():
-                # TODO: should we add the seen value to the failure?
-                if not k or not k[0].isalpha():
-                    # We only want sane variable names. Exclude anything that starts with a symbol or non-alpha
-                    continue
-                if None in v and k not in env_accesses:
-                    env_accesses.append(k)
-
-        # for env in env_accesses:
-        #    if not (2 < len(env) < 16):
-        #        print(f"Skipping unset env {env} since it's too long or too short")
-        default_env_vars = DEFAULT_ENV_VARS
-        if "env" in config:
-            # Track the set env variables so we know they're set
-            default_env_vars += list(config["env"].keys())
-
-        env_failures = [
-            Failure("unset_" + env, self.ANALYSIS_TYPE, {"var": env, "source": "unset"}, patch_name=env)
-            for env in env_accesses
-            if 2 < len(env) < 16 and env not in default_env_vars
-        ]
-
-        # Another failure class is MTD accesses - if we have anything in env_mtd.txt
-        # we'll add those as failures too. TODO
-        # if isfile(pjoin(output_dir, mtd_output)):
-        #    with open(pjoin(output_dir, mtd_output)) as f:
-        #        mtd_accesses = [x.strip() for x in f.readlines() if len(x.strip()) > 0]
-        #        # Value is only used for pretty-printing here
-        #        env_failures += [Failure('mtd_' + mtd, self.ANALYSIS_TYPE, {'var': mtd, 'value':'[mtd]', 'source': 'mtd'})
-        #                         for mtd in mtd_accesses]
-
-        return env_failures
-
-    def get_potential_mitigations(self, config, failure: Failure) -> List[Mitigation]:
-        # If we just ran a dynamic search that's the only mitigation we'll apply
-        # Expect failure_type to be envone, not env?
-
-        fail_info = failure.info
-        var_name = fail_info["var"]
-
-        if config and any(
-            v == ENV_MAGIC_VAL for k, v in config[self.ANALYSIS_TYPE].items()
-        ):
-            results = []
-            if fail_info["source"] != "dynamic":
-                # Should only be here after a dynamic search
-                for k, v in config[self.ANALYSIS_TYPE].items():
-                    if v == ENV_MAGIC_VAL:
-                        print(f"Found magic value {v} in {k} but ERROR")
-                print(fail_info)
-                raise ValueError(
-                    f"Expected source=dynamic for config with {ENV_MAGIC_VAL} but got {fail_info}"
-                )
-
-            if len(fail_info["values"]) > 0:
-                # If we found some dynamic values, those are our mitigations!
-                # We'll have a base weight of 20 and we'll add up to 50 depending on length
-                for dynval in fail_info["values"]:
-                    if len(dynval) < 1:
-                        continue
-                    if len(dynval) > 16:
-                        # Warn but skip
-                        print(
-                            f"Ignoring potential dynval {dynval} since it's quite long"
-                        )
-                        continue
-
-                    weight = 10 + max(min(20, len(dynval)), 90)
-                    results.append(
-                        Mitigation(
-                            dynval,
-                            self.ANALYSIS_TYPE,
-                            {
-                                "value": dynval,
-                                "var": var_name,
-                                "weight": weight,
-                                "source": "from_dynamic",
-                            },
-                            patch={'env': {var_name: dynval}},
-                            failure_name=failure.friendly_name
-                        )
-                    )
-            else:
-                # Otherwise, dynamic search failed. If we still see varname as 'unset' in our failure log,
-                # it's not being controlled by the kernel boot args - we should store this in our global
-                # state and move on. (TODO). For now we'll just add some defaults whenever we don't get
-                # results from dynamic, but if we were smarter we'd be able to give up on un-settable vars
-                for val in self.DEFAULT_VALUES:
-                    results.append(
-                        Mitigation(
-                            val,
-                            self.ANALYSIS_TYPE,
-                            {
-                                "value": val,
-                                "var": var_name,
-                                "weight": 0.1,
-                                "source": "default",
-                            },
-                            patch={'env': {var_name: val}},
-                            failure_name=failure.friendly_name
-                        )
-                    )
-            return results
-
-        # If we get here we're NOT doing a dynamic search.
-        existing_vars = list(config[self.ANALYSIS_TYPE].keys()) if config else []
-        if var_name in existing_vars:
-            # Can't mitigate an unset variable that's already set by our config. If it was magic
-            # value, we would've handled above. But we're here so it must be set to a concrete value
-            # raise ValueError(f"{var_name} was already set but it was also our failure - what's happening")
-            return []
-
-        # Otherwise: variable was unset. The only mitigation we can propose here is to try magic values.
-        # If that fails, we'll add some defaults
-        return [
-            Mitigation(
-                "magic_" + var_name,
-                self.ANALYSIS_TYPE,
-                {
-                    "value": ENV_MAGIC_VAL,
-                    "var": var_name,
-                    "weight": 1,
-                    "source": "need_dynamic",
-                },
-                patch={'env': {var_name: ENV_MAGIC_VAL}},
-                exclusive=self.ANALYSIS_TYPE,
-                failure_name=failure.friendly_name,
-            )
-        ]
-
-    def implement_mitigation(
-        self, config: Configuration, failure: Failure, mitigation: Mitigation
-    ) -> List[Configuration]:
-        # Given a mitigation, add it to a copy of the config and return
-        name = f'{mitigation.info["var"]}={mitigation.info["value"][:4]}'
-
-        assert (
-            mitigation.type == self.ANALYSIS_TYPE
-        ), f"Unexpected mitigation type: {mitigation.type}"
-
-        # Properties are the parent's plus we set the variable to the mitigation value
-        new_props = deepcopy(config.info)
-        new_props[self.ANALYSIS_TYPE][mitigation.info["var"]] = mitigation.info["value"]
-
-        exclusive = None
-
-        if mitigation.info["source"] == "need_dynamic":
-            # Exclusive node!
-            exclusive = self.ANALYSIS_TYPE
-        return [Configuration(name, new_props, exclusive=exclusive)]
