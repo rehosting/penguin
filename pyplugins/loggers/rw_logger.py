4847b7aef0fe68db34b41292346c4789741adc81
Caden Kline <caden.kline@ll.mit.edu>
2025-06-17 11:20:55 -0400

    async order of operations fix

--- pyplugins/loggers/rw_logger.py
+++ pyplugins/loggers/rw_logger.py
@@ -15,7 +15,7 @@ class RWLog(PyPlugin):
     def write(self, cpu, proto, syscall, fd, buf, count):
         s = yield from plugins.portal.read_str(buf)
         signed_fd = int(self.panda.ffi.cast("target_long", fd))
-        fname = yield from plugins.portal.get_fd_name(fd) or "?"
+        fname = (yield from plugins.portal.get_fd_name(fd)) or "?"
         args = yield from plugins.portal.get_args()
         if args:
             procname = args[0]
@@ -34,7 +34,7 @@ class RWLog(PyPlugin):
     def read(self, cpu, proto, syscall, fd, buf, count):
         s = yield from plugins.portal.read_str(buf)
         signed_fd = int(self.panda.ffi.cast("target_long", fd))
-        fname = yield from plugins.portal.get_fd_name(fd) or "?"
+        fname = (yield from plugins.portal.get_fd_name(fd)) or "?"
         # Get name of FD, if it's valid
         signed_fd = int(self.panda.ffi.cast("target_long", fd))
         args = yield from plugins.portal.get_args()
