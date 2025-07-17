22836b8ce8c7a804f19ba67913488b17150173c3
Luke Craig <lacraig3@gmail.com>
2025-07-17 13:15:51 -0400

    penguin_run: make shared_dir default

--- src/penguin/penguin_run.py
+++ src/penguin/penguin_run.py
@@ -18,6 +18,7 @@ from .common import yaml
 from .defaults import default_plugin_path, vnc_password
 from penguin.penguin_config import load_config
 from .utils import hash_image_inputs
+from .plugin_manager import ArgsBox
 
 
 # Note armel is just panda-system-arm and mipseb is just panda-system-mips
@@ -435,23 +436,22 @@ def run_config(
             "-display", "none",
         ]  # ttyS0: guest console output
 
-    if "shared_dir" in conf["core"]:
-        shared_dir = conf["core"]["shared_dir"]
-        if shared_dir[0] == "/":
-            shared_dir = shared_dir[1:]  # Ensure it's relative path to proj_dir
-        shared_dir = os.path.join(out_dir, shared_dir)
-        os.makedirs(shared_dir, exist_ok=True)
-        args += [
-            "-virtfs",
-            ",".join(
-                (
-                    "local",
-                    f"path={shared_dir}",
-                    "mount_tag=igloo_shared_dir",
-                    "security_model=mapped-xattr",
-                )
-            ),
-        ]
+    shared_dir = conf["core"].get("shared_dir", f"{out_dir}/shared")
+    if shared_dir[0] == "/":
+        shared_dir = shared_dir[1:]  # Ensure it's relative path to proj_dir
+    shared_dir = os.path.join(out_dir, shared_dir)
+    os.makedirs(shared_dir, exist_ok=True)
+    args += [
+        "-virtfs",
+        ",".join(
+            (
+                "local",
+                f"path={shared_dir}",
+                "mount_tag=igloo_shared_dir",
+                "security_model=mapped-xattr",
+            )
+        ),
+    ]
 
     args = args + console_out + root_shell
 
