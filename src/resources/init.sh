96d4197f5491a21e13c60fa66dfdf17d00f0050a
Luke Craig <lacraig3@gmail.com>
2025-07-30 17:45:55 -0400

    start penguin with /igloo/preinit before init

--- src/resources/init.sh
+++ src/resources/init.sh
@@ -1,12 +1,5 @@
 #!/igloo/utils/busybox sh
 
-/igloo/utils/busybox mkdir /igloo/shared
-echo '[IGLOO INIT] Mounting shared directory';
-/igloo/utils/busybox mount -t 9p -o trans=virtio igloo_shared_dir /igloo/shared -oversion=9p2000.L,posixacl,msize=8192000
-# /igloo/shared/host_files/gen_live_image
-/igloo/shared/host_files/send_hypercall_raw 0xf113c0df
-/igloo/utils/busybox sh /igloo/shared/host_files/gen_live_image.sh
-
 # First, source all the scripts in /igloo/source.d
 for f in /igloo/source.d/*; do
   source $f
