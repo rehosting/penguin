4511f282ab34b30241e28cf8a049a2f9bbe92b34
Luke Craig <lacraig3@gmail.com>
2025-08-07 17:08:41 -0400

    refactor: update Uprobes and LiveImage for filesystem initialization and hypercall handling

--- src/resources/preinit.sh
+++ src/resources/preinit.sh
@@ -4,5 +4,6 @@
 echo '[IGLOO INIT] Mounting shared directory';
 /igloo/utils/busybox mount -t 9p -o trans=virtio igloo_shared_dir /igloo/shared -oversion=9p2000.L,posixacl,msize=8192000
 /igloo/shared/host_files/send_hypercall_raw 0xf113c0df
-/igloo/utils/busybox sh /igloo/shared/host_files/gen_live_image.sh
+/igloo/utils/busybox sh /igloo/shared/host_files/gen_live_image.sh > /igloo/shared/host_files/live_image_guest.log 2>&1
+/igloo/shared/host_files/send_hypercall_raw 0xf113c1df
 exec /igloo/init
