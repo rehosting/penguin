4d7017d39dee49a137fb500f7393e629e18e98ed
Luke Craig <lacraig3@gmail.com>
2025-09-16 14:17:12 -0400

    penguin_run: simplify with new q_config file

--- src/penguin/penguin_run.py
+++ src/penguin/penguin_run.py
@@ -19,70 +19,7 @@ from .defaults import default_plugin_path, vnc_password
 from penguin.penguin_config import load_config
 from .plugin_manager import ArgsBox
 from .utils import hash_image_inputs
-
-
-# Note armel is just panda-system-arm and mipseb is just panda-system-mips
-ROOTFS = "/dev/vda"  # Common to all
-qemu_configs = {
-    "armel": {
-        "qemu_machine": "virt",
-        "arch": "arm",
-        "kernel_whole": "zImage.armel",
-    },
-    "aarch64": {
-        "qemu_machine": "virt",
-        "kconf_group": "arm64",
-        "cpu": "cortex-a57",
-        "kernel_whole": "zImage.arm64",
-    },
-    "loongarch64": {
-        "qemu_machine": "virt",
-        "cpu": "la464",
-        "kernel_fmt": "vmlinuz.efi"
-    },
-    "mipsel": {
-        "qemu_machine": "malta",
-    },
-    "mipseb": {
-        "qemu_machine": "malta",
-        "arch": "mips",
-        "kernel_whole": "vmlinux.mipseb"
-    },
-    "mips64el": {
-        "qemu_machine": "malta",
-        "cpu": "MIPS64R2-generic",
-        "kernel_whole": "vmlinux.mips64el",
-    },
-    "mips64eb": {
-        "qemu_machine": "malta",
-        "arch": "mips64",
-        "kernel_whole": "vmlinux.mips64eb",
-        "cpu": "MIPS64R2-generic",
-    },
-    "powerpc64el": {
-        "qemu_machine": "pseries",
-        "arch": "ppc64",
-        "cpu": "power9",
-    },
-    "powerpc64": {
-        "qemu_machine": "pseries",
-        "arch": "ppc64",
-        "cpu": "power9",
-        "kconf_group": "powerpc64",
-        "kernel_whole": "vmlinux.powerpc64"
-    },
-    "riscv64": {
-        "qemu_machine": "virt",
-        "kernel_fmt": "Image",
-    },
-    "intel64": {
-        "qemu_machine": "pc",
-        "arch": "x86_64",
-        "kconf_group": "x86_64",
-        "kernel_fmt": "bzImage",
-    },
-}
-
+from .q_config import load_q_config, ROOTFS
 
 @contextmanager
 def print_to_log(out, err):
