8656662d167bd19407ac0a00dabbbbf2057e0f71
Benjamin Levy <benjamin.levy@ll.mit.edu>
2025-10-01 15:35:58 -0400

    config: remove type equality assert on patch merge

--- src/penguin/common.py
+++ src/penguin/common.py
@@ -63,8 +63,6 @@ def patch_config(logger, base_config, patch):
         if new is None:
             return base
 
-        assert type(base) is type(new)
-
         if hasattr(base, "merge"):
             return base.merge(new)
 
