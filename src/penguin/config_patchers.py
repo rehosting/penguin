4ea8ffb58102ad0ca5565e1c6cf07e69e2c1ecbc
Zak Estrada <zachary.estrada@ll.mit.edu>
2025-10-20 09:35:42 -0400

    ppc dylibs fixup

--- src/penguin/config_patchers.py
+++ src/penguin/config_patchers.py
@@ -429,8 +429,8 @@ class BasePatch(PatchGenerator):
             self.dylib_dir = "x86_64"
         elif arch_identified == "loongarch64":
             self.dylib_dir = "loongarch"
-        elif arch_identified in ["powerpc64", "powerpc64el"]:
-            self.dylib_dir = "ppc64"
+        elif "powerpc" in self.arch_name:
+            self.dylib_dir = self.arch_name.replace("powerpc", "ppc")  # dylibs are built with short names
         else:
             self.dylib_dir = self.arch_dir
 
