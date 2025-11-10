c5dbe4cb07dda68eefbd8a0471af5ccc98d372b2
Zak Estrada <zachary.estrada@ll.mit.edu>
2025-11-09 21:50:44 -0500

    add --bool option for config reader

--- src/resources/source.d/55_force_www.sh
+++ src/resources/source.d/55_force_www.sh
@@ -1,4 +1,4 @@
-if [ ! -z "${WWW}" ]; then
+if [ /igloo/utils/get_config --bool core.force_www ]; then
   if [ -e /igloo/utils/www_cmds ]; then
     echo '[IGLOO INIT] Force-launching webserver commands';
     /igloo/utils/sh /igloo/utils/www_cmds &
