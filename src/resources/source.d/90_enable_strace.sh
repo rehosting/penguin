9453bbf5e37e45d3704485747d7bddc97dafcea6
Zak Estrada <zachary.estrada@ll.mit.edu>
2025-11-10 17:12:55 -0500

    replace environment variables in guest with hypercall for config options

--- src/resources/source.d/90_enable_strace.sh
+++ src/resources/source.d/90_enable_strace.sh
@@ -1,6 +1,5 @@
-if [ ! -z "${STRACE}" ]; then
+if /igloo/utils/get_config core.strace > /dev/null 2>&1; then
   # Strace init in the background (to follow through the exec)
   /igloo/utils/sh -c "/igloo/utils/strace -f -p 1" &
   /igloo/utils/sleep 1
-  unset STRACE
 fi
