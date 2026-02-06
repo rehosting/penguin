40d77e19ec54b73132dcab456785f08006eaca50
Luke Craig <lacraig3@gmail.com>
2026-02-06 15:50:58 -0500

    add cli commands from penguin_ctrl

---
+++ pengutils/pengutils/utils/util_events.py
@@ -0,0 +1,32 @@
+import socket
+from os import environ as env
+from os.path import join, isabs, dirname
+from typing import Optional
+import json
+
+def in_container() -> bool:
+    return env.get("PENGUIN_PROJECT_DIR", None) != None
+
+
+def send_command(data:dict=None, sock:Optional[str]=None) -> Optional[dict]:
+    if sock is None:
+        if in_container():
+            sock = join(env["PENGUIN_PROJECT_DIR"], "results", "latest", "penguin.sock")
+        else:
+            raise Exception("Socket path required")
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as c:
+        c.connect(sock); c.sendall(json.dumps(data).encode()); c.shutdown(socket.SHUT_WR)
+        data = b""
+        while True:
+            chunk = c.recv(4096)
+            if not chunk: break
+            data += chunk
+        
+        if not data:
+            return
+        try:
+            resp = json.loads(data.decode())
+        except json.JSONDecodeError:
+            print("Error decoding response:", data.decode())
+            return
+        return resp
