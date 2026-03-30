0640aa4d8e16a4abfd970d8f8347bac6b1fdbab8
Luke Craig <lacraig3@gmail.com>
2026-03-30 16:38:19 -0400

    netdevs: make better tests

--- pyplugins/testing/netdevs_test.py
+++ pyplugins/testing/netdevs_test.py
@@ -4,9 +4,9 @@ from apis.net import Netdev
 kffi = plugins.kffi
 mem = plugins.mem
 
-class BlahNetDev(Netdev):
-    def setup(self, device_name: str, netdev):
-        self.logger.info(f"Setting up altered network device {device_name}")
+class SampleNetDev(Netdev):
+    def setup(self, netdev):
+        self.logger.info(f"Setting up altered network device {self.name}")
         netdev_ops = yield from kffi.deref(netdev.netdev_ops)
         netdev_ops.ndo_do_ioctl = yield from kffi.callback(self.ioctl_handler)
         netdev_ops.ndo_get_stats64 = yield from kffi.callback(self.stats64_handler)
@@ -30,6 +30,36 @@ class BlahNetDev(Netdev):
         self.logger.info((name, args, cmd))
         return 0
 
+class SampleNetDev2(Netdev):
+    def setup(self, netdev):
+        self.logger.info(f"Setting up sample network device {self.name}")
+        netdev_ops = yield from kffi.deref(netdev.netdev_ops)
+        netdev_ops.ndo_do_ioctl = yield from kffi.callback(self.ioctl_handler)
+        netdev_ops.ndo_get_stats64 = yield from kffi.callback(self.stats64_handler)
+        yield from mem.write_bytes(netdev.netdev_ops.address, bytes(netdev_ops))
+    
+    def stats64_handler(self, pt_regs, netdev_ptr, stats64_ptr):
+        netdevs = yield from kffi.read_type(netdev_ptr, "net_device")
+        stats64 = yield from kffi.read_type(stats64_ptr, "rtnl_link_stats64")
+        self.logger.info(f"Getting stats64 for device {kffi.ffi.string(netdevs.name)}")
+        stats64.rx_packets = 7331
+        stats64.tx_packets = 8331
+        stats64.rx_bytes = 9331
+        stats64.tx_bytes = 431
+        # Just return zeroed stats for now
+        yield from mem.write_bytes(stats64_ptr, bytes(stats64))
+    
+    def ioctl_handler(self, pt_regs, netdev_ptr, ifreq_ptr, cmd):
+        args = yield from plugins.osi.get_args()
+        netdevs = yield from kffi.read_type(netdev_ptr, "net_device")
+        name = kffi.ffi.string(netdevs.name)
+        self.logger.info((name, args, cmd))
+        return 0
+
 class NetworkDeviceTest(Plugin):
     def __init__(self):
-        plugins.net.register_netdev("blah0", BlahNetDev)
+        # Register some sample netdevs for testing
+        # Register is an alias for register_netdev, so both should work
+        plugins.net.register("sample0", SampleNetDev)
+        plugins.net.register_netdev("sample1", SampleNetDev)
+        plugins.net.register_netdev("sample2", SampleNetDev2)
