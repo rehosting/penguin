f2da92751b8dedfb2a8f9a75bf139d6953419cd4
Zak Estrada <Zachary.Estrada@ll.mit.edu>
2026-05-16 18:06:42 -0400

    trying different spot for patch

--- tests/unit_tests/basic_target/init.d/load_libinject.sh
+++
@@ -1,5 +0,0 @@
-#!/igloo/utils/sh
-# Force lib_inject.so to load by invoking a dynamic /igloo/utils binary with
-# LD_PRELOAD set. The constructor inside lib_inject.d/dropin_lib.c then writes
-# its marker file. We don't care about the binary's exit status.
-LD_PRELOAD=/igloo/dylibs/lib_inject.so /igloo/utils/test_nvram get __unused_libinject_loader__ >/dev/null 2>&1 || /busybox true
