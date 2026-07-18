678554ebaf45c469a0641ed971e64a95f503f83a
lacraig2 <lacraig2@users.noreply.github.com>
2026-07-18 15:33:44 +0000

    deploy: bf9b8456035dfd2b54b6b536a33dd7bf83116a77

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'v3.0.74.dev0+gbf9b84560.d20260718',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
